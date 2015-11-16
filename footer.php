c8d0111546fb79068d35a1e25beda9a43c212b28
Greg Rickaby <gregrickaby@gmail.com>
2015-11-16 10:08:37 -0600

    remove role="contentinfo". fixes #113

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<?php wds_page_builder_area( 'after_content' ); ?>
 	</div>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	<footer id="colophon" class="site-footer">
 		<div class="wrap">
 
 			<div class="site-info">
