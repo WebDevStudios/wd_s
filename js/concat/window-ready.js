8f6ae49403bd68401fadaa444f7e1ddd1f24e04d
Greg Rickaby <gregrickaby@gmail.com>
2015-11-25 13:09:11 -0600

    add window "ready" support. fixes #126

---
+++ js/concat/window-ready.js
@@ -0,0 +1,34 @@
+/**
+ * After everything has loaded, add a "ready" class to <body>.
+ */
+window.Window_Ready = {};
+( function( window, $, that ) {
+
+	// Constructor
+	that.init = function() {
+		that.cache();
+		that.bindEvents();
+	};
+
+	// Cache document elements
+	that.cache = function() {
+		that.$c = {
+			window: $(window),
+			body: $(document.body),
+		};
+	};
+
+	// Combine all events
+	that.bindEvents = function() {
+		that.$c.window.load( that.addBodyClass );
+	};
+
+	// Add a class to <body>
+	that.addBodyClass = function() {
+		that.$c.body.addClass( 'ready' );
+	};
+
+	// Engage
+	$( that.init );
+
+})( window, jQuery, window.Window_Ready );
