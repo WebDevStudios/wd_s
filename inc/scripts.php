0fe4d34947ca892c866cef44345405c7705642e2
Greg Rickaby <gregrickaby@gmail.com>
2015-12-21 09:01:49 -0600

    move assets into new /assets dir

--- inc/scripts.php
+++ inc/scripts.php
@@ -63,7 +63,7 @@ function _s_scripts() {
 	wp_enqueue_style( '_s-google-font', _s_font_url(), array(), null );
 	wp_enqueue_style( '_s-style', get_stylesheet_directory_uri() . '/style' . $suffix . '.css', array(), $version );
 
-	wp_enqueue_script( '_s-project', get_template_directory_uri() . '/js/project' . $suffix . '.js', array( 'jquery' ), $version, true );
+	wp_enqueue_script( '_s-project', get_template_directory_uri() . '/assets/js/project' . $suffix . '.js', array( 'jquery' ), $version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -99,7 +99,7 @@ endif;
 function _s_include_svg_definitions() {
 
 	// Define svg sprite file
-	$svg_defs = get_template_directory() . '/images/svg-defs.svg';
+	$svg_defs = get_template_directory() . '/assets/images/svg-defs.svg';
 
 	// If it exsists, include it
 	if ( file_exists( $svg_defs ) ) {
