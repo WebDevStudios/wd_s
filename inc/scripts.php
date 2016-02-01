1d62494eafbaefecdc3097c3dcf92a80ad9e3dac
Greg Rickaby <gregrickaby@gmail.com>
2016-02-01 09:13:24 -0600

    fix merge conflict

--- inc/scripts.php
+++ inc/scripts.php
@@ -64,12 +64,8 @@ function _s_scripts() {
 	wp_enqueue_style( 'animate.css' );
 	wp_enqueue_style( '_s-style', get_stylesheet_directory_uri() . '/style' . $suffix . '.css', array(), $version );
 
-<<<<<<< HEAD
-	wp_enqueue_script( '_s-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), $version, true );
-=======
 	// Enqueue scripts.
-	wp_enqueue_script( '_s-project', get_template_directory_uri() . '/assets/js/project' . $suffix . '.js', array( 'jquery' ), $version, true );
->>>>>>> master
+	wp_enqueue_script( '_s-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), $version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -104,20 +100,11 @@ endif;
  */
 function _s_include_svg_icons() {
 
-<<<<<<< HEAD
 	// Define SVG sprite file
 	$svg_icons = get_template_directory() . '/assets/images/svg-icons.svg';
 
 	// If it exsists, include it
 	if ( file_exists( $svg_icons ) ) {
 		require_once( $svg_icons );
-=======
-	// Define SVG sprite file.
-	$svg_defs = get_template_directory() . '/assets/images/svg-defs.svg';
-
-	// If it exsists, include it.
-	if ( file_exists( $svg_defs ) ) {
-		require_once( $svg_defs );
->>>>>>> master
 	}
 }
