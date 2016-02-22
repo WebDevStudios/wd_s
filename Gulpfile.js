c9a432a786d3a54c8a740b0bdc8dd602fcf823b8
Damon Cook <damonacook@gmail.com>
2016-02-22 15:57:01 -0500

    svg task triggers clean:icons task

--- Gulpfile.js
+++ Gulpfile.js
@@ -141,13 +141,15 @@ gulp.task('sass:lint', ['cssnano'], function () {
 gulp.task('clean:icons', function() {
 	return del(['assets/images/svg-icons.svg']);
 });
+
+/**
  * Minify, concatenate, and clean SVG icons.
  *
  * https://www.npmjs.com/package/gulp-svgmin
  * https://www.npmjs.com/package/gulp-svgstore
  * https://www.npmjs.com/package/gulp-cheerio
  */
-gulp.task('svg', function() {
+gulp.task('svg', ['clean:icons'], function() {
 	return gulp.src(paths.icons)
 	.pipe(plumber({ errorHandler: handleErrors }))
 	.pipe(svgmin())
@@ -283,11 +285,6 @@ gulp.task('watch', function() {
 });
 
 /**
- * Delete compiled files.
- */
-gulp.task('clean:icons', function() {
-	return del(['assets/images/svg-icons.svg']);
-});
 
 gulp.task('clean:styles', function() {
 	return del(['style.css', 'style.min.css']);
@@ -305,8 +302,8 @@ gulp.task('clean:pot', function() {
  * Create indivdual tasks.
  */
 gulp.task('i18n', ['clean:pot','wp-pot']);
-gulp.task('icons', ['clean:icons', 'svg']);
 gulp.task('styles', ['clean:styles', 'postcss', 'cssnano', 'sass:lint']);
 gulp.task('scripts', ['clean:scripts', 'uglify']);
+gulp.task('icons', ['svg']);
 gulp.task('sprites', ['imagemin', 'spritesmith']);
 gulp.task('default', ['i18n','icons', 'styles', 'scripts', 'sprites']);
