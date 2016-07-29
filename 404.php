653072c037a512a6ee4b482e2ca8311128513eb9
Greg Rickaby <gregrickaby@gmail.com>
2016-07-29 12:51:31 -0500

    Create basic styles for ancillary pages. Fixes #175. (Props @ImBigWill)

--- 404.php
+++ 404.php
@@ -15,52 +15,21 @@ get_header(); ?>
 
 				<section class="error-404 not-found">
 					<header class="page-header">
-						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', '_s' ); ?></h1>
+						<h1 class="page-title"><?php esc_html_e( 'Sorry, this page doesn\'t exist.', '_s' ); ?></h1>
 					</header><!-- .page-header -->
 
 					<div class="page-content">
-						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', '_s' ); ?></p>
 
-						<?php
-							get_search_form();
+						<p><?php esc_html_e( 'It seems we can’t find what you’re looking for. Perhaps searching can help.', '_s' ); ?></p>
 
-							the_widget( 'WP_Widget_Recent_Posts' );
-
-							// Only show the widget if site has multiple categories.
-							if ( _s_categorized_blog() ) :
-						?>
-
-						<div class="widget widget_categories">
-							<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', '_s' ); ?></h2>
-							<ul>
-							<?php
-								wp_list_categories( array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								) );
-							?>
-							</ul>
-						</div><!-- .widget -->
-
-						<?php
-							endif;
-
-							/* translators: %1$s: smiley */
-							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', '_s' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-							the_widget( 'WP_Widget_Tag_Cloud' );
-						?>
+						<?php get_search_form(); ?>
 
 					</div><!-- .page-content -->
 				</section><!-- .error-404 -->
 
 			</main><!-- #main -->
 		</div><!-- .primary -->
+
 	</div><!-- .wrap -->
 
-<?php
-get_footer();
+<?php get_footer(); ?>
