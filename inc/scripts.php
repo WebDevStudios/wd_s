4533f82a80d1d2a499d8614db0664e308b0c0d4e
Greg Rickaby <gregrickaby@gmail.com>
2016-08-11 12:20:14 -0500

    change include path to /scripts

--- inc/scripts.php
+++ inc/scripts.php
@@ -70,7 +70,7 @@ function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_stylesheet_directory_uri() . '/style' . $suffix . '.css', array(), $version );
 
 	// Enqueue scripts.
-	wp_enqueue_script( '_s-scripts', get_template_directory_uri() . '/assets/js/project' . $suffix . '.js', array( 'jquery' ), $version, true );
+	wp_enqueue_script( '_s-scripts', get_template_directory_uri() . '/assets/scripts/project' . $suffix . '.js', array( 'jquery' ), $version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -78,7 +78,7 @@ function _s_scripts() {
 
 	// Enqueue the mobile nav script
 	// Since we're showing/hiding based on CSS and wp_is_mobile is wp_is_imperfect, enqueue this everywhere.
-	wp_enqueue_script( '_s-mobile-nav', get_template_directory_uri() . '/assets/js/mobile-nav-menu' . $suffix . '.js', array( 'jquery' ), $version, true );
+	wp_enqueue_script( '_s-mobile-nav', get_template_directory_uri() . '/assets/scripts/mobile-nav-menu' . $suffix . '.js', array( 'jquery' ), $version, true );
 }
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
 
