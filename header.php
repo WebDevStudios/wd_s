6bcaa19ec4e6c1118703840a0e33f17033f36554
Rich Edmunds <rich@webdevstudios.com>
2016-09-30 10:43:10 -0600

    Add children of <head> element indent

--- header.php
+++ header.php
@@ -9,18 +9,19 @@
  * @package _s
  */
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<?php global $is_IE; if ( $is_IE ) : ?>
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<?php endif; ?>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php global $is_IE; if ( $is_IE ) : ?>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<?php endif; ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
