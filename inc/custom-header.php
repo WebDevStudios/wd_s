f6adcf1c20568a028205a56eafa44b4bfdb5680e
Corey M Collins <coreymcollins@gmail.com>
2016-11-05 09:26:36 -0400

    PHPCS Cleanup of Theme (minus template-tags.php) (#219)
    * Adjust nesting of while statement to alleviate error message on closing endwhile
    
    * Update output of comment heading to fix PHPCS error and be consistent with using numbers instead of words
    
    * Sanitize our footer copyright and menu HTML
    
    * Cleanup PHPCS errors in header.php
    
    * Cleanup indentation
    
    * Add an opening comment to the searchform.php template
    
    * PHPCS cleanup of functions.php
    
    * Fix spacing issue for PHPCS
    
    * Cleanup spacing and indentations
    
    * Spacing adjustments for PHPCS cleanup
    
    * PHPCS cleanup of spacing and allowed overrides in extras file

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -37,91 +37,90 @@ function _s_custom_header_setup() {
 add_action( 'after_setup_theme', '_s_custom_header_setup' );
 
 if ( ! function_exists( '_s_header_style' ) ) :
-/**
- * Styles the header image and text displayed on the blog.
- *
- * @see _s_custom_header_setup().
- */
-function _s_header_style() {
-	$header_text_color = get_header_textcolor();
+	/**
+	 * Styles the header image and text displayed on the blog.
+	 *
+	 * @see _s_custom_header_setup().
+	 */
+	function _s_header_style() {
+		$header_text_color = get_header_textcolor();
 
-	// If no custom options for text are set, let's bail.
-	// get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text (returns 'blank') or any hex value.
-	if ( HEADER_TEXTCOLOR === $header_text_color ) {
-		return;
-	}
+		// If no custom options for text are set, let's bail.
+		// get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text (returns 'blank') or any hex value.
+		if ( HEADER_TEXTCOLOR === $header_text_color ) {
+			return;
+		}
 
-	// If we get this far, we have custom styles. Let's do this.
-	?>
-	<style type="text/css">
-	<?php
+		// If we get this far, we have custom styles. Let's do this.
+		?>
+		<style type="text/css">
+		<?php
 		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-	?>
-		.site-title,
-		.site-description {
-			position: absolute;
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text use that.
-		else :
-	?>
-		.site-title a,
-		.site-description {
-			color: #<?php echo esc_attr( $header_text_color ); ?>;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
-}
+		if ( ! display_header_text() ) : ?>
+			.site-title,
+			.site-description {
+				position: absolute;
+				clip: rect(1px, 1px, 1px, 1px);
+			}
+		<?php
+			// If the user has set a custom color for the text use that.
+			else :
+		?>
+			.site-title a,
+			.site-description {
+				color: #<?php echo esc_attr( $header_text_color ); ?>;
+			}
+		<?php endif; ?>
+		</style>
+		<?php
+	}
 endif; // _s_header_style
 
 if ( ! function_exists( '_s_admin_header_style' ) ) :
-/**
- * Styles the header image displayed on the Appearance > Header admin panel.
- *
- * @see _s_custom_header_setup().
- */
-function _s_admin_header_style() {
-?>
-	<style type="text/css">
-		.appearance_page_custom-header #headimg {
-			border: none;
-		}
-		#headimg h1,
-		#desc {
-		}
-		#headimg h1 {
-		}
-		#headimg h1 a {
-		}
-		#desc {
-		}
-		#headimg img {
-		}
-	</style>
-<?php
-}
+	/**
+	 * Styles the header image displayed on the Appearance > Header admin panel.
+	 *
+	 * @see _s_custom_header_setup().
+	 */
+	function _s_admin_header_style() {
+	?>
+		<style type="text/css">
+			.appearance_page_custom-header #headimg {
+				border: none;
+			}
+			#headimg h1,
+			#desc {
+			}
+			#headimg h1 {
+			}
+			#headimg h1 a {
+			}
+			#desc {
+			}
+			#headimg img {
+			}
+		</style>
+	<?php
+	}
 endif; // _s_admin_header_style
 
 if ( ! function_exists( '_s_admin_header_image' ) ) :
-/**
- * Custom header image markup displayed on the Appearance > Header admin panel.
- *
- * @see _s_custom_header_setup().
- */
-function _s_admin_header_image() {
-?>
-	<div id="headimg">
-		<h1 class="displaying-header-text">
-			<a id="name" style="<?php echo esc_attr( 'color: #' . get_header_textcolor() ); ?>" onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-		</h1>
-		<div class="displaying-header-text" id="desc" style="<?php echo esc_attr( 'color: #' . get_header_textcolor() ); ?>"><?php bloginfo( 'description' ); ?></div>
-		<?php if ( get_header_image() ) : ?>
-		<img src="<?php header_image(); ?>" alt="">
-		<?php endif; ?>
-	</div>
-<?php
-}
+	/**
+	 * Custom header image markup displayed on the Appearance > Header admin panel.
+	 *
+	 * @see _s_custom_header_setup().
+	 */
+	function _s_admin_header_image() {
+	?>
+		<div id="headimg">
+			<h1 class="displaying-header-text">
+				<a id="name" style="<?php echo esc_attr( 'color: #' . get_header_textcolor() ); ?>" onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+			</h1>
+			<div class="displaying-header-text" id="desc" style="<?php echo esc_attr( 'color: #' . get_header_textcolor() ); ?>"><?php bloginfo( 'description' ); ?></div>
+			<?php if ( get_header_image() ) : ?>
+			<img src="<?php header_image(); ?>" alt="">
+			<?php endif; ?>
+		</div>
+	<?php
+	}
 endif; // _s_admin_header_image
--- inc/extras.php
+++ inc/extras.php
@@ -15,12 +15,15 @@
  */
 function _s_body_classes( $classes ) {
 
+	// @codingStandardsIgnoreStart
+	// Allows for incorrect snake case like is_IE to be used without throwing errors.
 	global $is_IE;
 
 	// If it's IE, add a class.
 	if ( $is_IE ) {
 		$classes[] = 'ie';
 	}
+	// @codingStandardsIgnoreEnd
 
 	// Give all pages a unique class.
 	if ( is_page() ) {
@@ -38,9 +41,13 @@ function _s_body_classes( $classes ) {
 	}
 
 	// Are we on mobile?
+	// PHP CS wants us to use jetpack_is_mobile instead, but what if we don't have Jetpack installed?
+	// Allows for using wp_is_mobile without throwing an error.
+	// @codingStandardsIgnoreStart
 	if ( wp_is_mobile() ) {
 		$classes[] = 'mobile';
 	}
+	// @codingStandardsIgnoreEnd
 
 	// Adds "no-js" class. If JS is enabled, this will be replaced (by javascript) to "js".
 	$classes[] = 'no-js';
