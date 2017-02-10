83464ccee8f88c31dafdb9c1ecb631af23eba3a8
Greg Rickaby <gregrickaby@gmail.com>
2017-02-10 14:41:12 -0600

    enable multiple tinymce editors

--- inc/customizer/tinymce.php
+++ inc/customizer/tinymce.php
@@ -10,19 +10,15 @@ if ( class_exists( 'WP_Customize_Control' ) ) :
 	/**
 	 * Class to create a custom text editor control
 	 */
-	class WDSMultipleTinyMceSupport extends WP_Customize_Control {
+	class _s_Text_Editor_Custom_Control extends WP_Customize_Control {
 
 		/**
 		 * Keep track of how many editors are added.
-		 *
-		 * @var integer
 		 */
 		protected static $count = 0;
 
 		/**
 		 * Render the content on the theme customizer page
-		 *
-		 * @return  void
 		 */
 		public function render_content() {
 			?>
@@ -31,26 +27,42 @@ if ( class_exists( 'WP_Customize_Control' ) ) :
 				<span class="description customize-control-description"><?php echo esc_html( $this->description ); ?></span>
 			</label>
 			<div class="wds-customize-text-editor">
-			<?php
-			// Register the editor.
-			wp_editor( $this->value(), $this->id, array(
-				'textarea_name' => $this->id,
-				'textarea_rows' => 4,
-				'media_buttons' => true,
-			) );
+				<?php
+				// Setttings for the editor.
+				$settings = array(
+					'textarea_name' => $this->id,
+					'textarea_rows' => 4,
+					'media_buttons' => true,
+				);
 
-			// Add the footer scripts.
-			$this->add_footer_scripts();
+				// Add the editor.
+				wp_editor( $this->value(), $this->id, $settings );
 
-			// Increment count.
-			++self::$count;
-			?>
+				// Only enqueue scripts once.
+				if ( 0 === self::$count ) {
+					$this->enqueue_scripts();
+				}
+
+				// add the footer scripts.
+				$this->add_footer_scripts();
+
+				// Increment count.
+				++self::$count;
+				?>
 			</div>
 			<?php
 		}
 
 		/**
-		 * Add footer scripts for the tinymce.
+		 * Enqueue scripts.
+		 */
+		protected function enqueue_scripts() {
+			wp_enqueue_script( 'tiny_mce' );
+			wp_enqueue_script( 'wds-customize-editor-js', get_template_directory_uri() . '/inc/customizer/assets/scripts/tinymce.js', array( 'jquery' ), '1.0.0', true );
+		}
+
+		/**
+		 * Add footer scripts for Tinymce.
 		 */
 		protected function add_footer_scripts() {
 			do_action( 'admin_print_footer_scripts' );
