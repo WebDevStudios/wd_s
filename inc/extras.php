bcb48ec8bd0660382be672f7744c0fb976a6a0a7
Greg Rickaby <gregrickaby@gmail.com>
2017-02-10 09:53:50 -0600

    move functions to their new homes

--- inc/extras.php
+++ inc/extras.php
@@ -8,102 +8,6 @@
  */
 
 /**
- * Adds custom classes to the array of body classes.
- *
- * @param array $classes Classes for the body element.
- * @return array
- */
-function _s_body_classes( $classes ) {
-
-	// @codingStandardsIgnoreStart
-	// Allows for incorrect snake case like is_IE to be used without throwing errors.
-	global $is_IE;
-
-	// If it's IE, add a class.
-	if ( $is_IE ) {
-		$classes[] = 'ie';
-	}
-	// @codingStandardsIgnoreEnd
-
-	// Give all pages a unique class.
-	if ( is_page() ) {
-		$classes[] = 'page-' . basename( get_permalink() );
-	}
-
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
-	}
-
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
-
-	// Are we on mobile?
-	// PHP CS wants us to use jetpack_is_mobile instead, but what if we don't have Jetpack installed?
-	// Allows for using wp_is_mobile without throwing an error.
-	// @codingStandardsIgnoreStart
-	if ( wp_is_mobile() ) {
-		$classes[] = 'mobile';
-	}
-	// @codingStandardsIgnoreEnd
-
-	// Adds "no-js" class. If JS is enabled, this will be replaced (by javascript) to "js".
-	$classes[] = 'no-js';
-
-	return $classes;
-}
-add_filter( 'body_class', '_s_body_classes' );
-
-/**
- * Add custom image sizes attribute to enhance responsive image functionality
- * for content images
- *
- * @package _s
- *
- * @param string $sizes A source size value for use in a 'sizes' attribute.
- * @param array  $size  Image size. Accepts an array of width and height
- *                      values in pixels (in that order).
- * @return string A source size value for use in a content image 'sizes' attribute.
- */
-function _s_content_image_sizes_attr( $sizes, $size ) {
-	$width = $size[0];
-
-	840 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 62vw, 840px';
-
-	if ( 'page' === get_post_type() ) {
-		840 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
-	} else {
-		840 > $width && 600 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 61vw, (max-width: 1362px) 45vw, 600px';
-		600 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
-	}
-
-	return $sizes;
-}
-add_filter( 'wp_calculate_image_sizes', '_s_content_image_sizes_attr', 10 , 2 );
-
-/**
- * Add custom image sizes attribute to enhance responsive image functionality
- * for post thumbnails
- *
- * @package _s
- *
- * @param array $attr Attributes for the image markup.
- * @param int   $attachment Image attachment ID.
- * @param array $size Registered image size or flat array of height and width dimensions.
- * @return string A source size value for use in a post thumbnail 'sizes' attribute.
- */
-function _s_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
-	if ( 'post-thumbnail' === $size ) {
-		is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 60vw, (max-width: 1362px) 62vw, 840px';
-		! is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 88vw, 1200px';
-	}
-	return $attr;
-}
-add_filter( 'wp_get_attachment_image_attributes', '_s_post_thumbnail_sizes_attr', 10 , 3 );
-
-/**
  * Returns true if a blog has more than 1 category.
  *
  * @return bool
@@ -132,19 +36,6 @@ function _s_categorized_blog() {
 		return false;
 	}
 }
-
-/**
- * Flush out the transients used in _s_categorized_blog.
- */
-function _s_category_transient_flusher() {
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-		return false;
-	}
-	// Like, beat it. Dig?
-	delete_transient( '_s_categories' );
-}
-add_action( 'delete_category', '_s_category_transient_flusher' );
-add_action( 'save_post',     '_s_category_transient_flusher' );
 
 /**
  * Get an attachment ID from it's URL.
@@ -196,8 +87,8 @@ function _s_get_attachment_id_from_url( $attachment_url = '' ) {
  *		@type string $text_color
  * }
  * @return string
- **/
-function _s_placeholder_image( $args = array() ) {
+ */
+function _s_get_placeholder_image( $args = array() ) {
 	$default_args = array(
 		'width'				=> '',
 		'height'			=> '',
@@ -253,8 +144,8 @@ function _s_placeholder_image( $args = array() ) {
  *		@type string $keywords Optional. Comma seperated list of keywords, such as: sailboat, water
  * }
  * @return string
- **/
-function _s_placeholder_unsplash( $args = array() ) {
+ */
+function _s_get_placeholder_unsplash( $args = array() ) {
 	$default_args = array(
 		'width'				=> '',
 		'height'			=> '',
