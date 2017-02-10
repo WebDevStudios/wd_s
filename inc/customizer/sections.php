5ed22811d194b8635c6dfe0b961f09c73bd7d23e
Greg Rickaby <gregrickaby@gmail.com>
2017-02-10 13:31:39 -0600

    create sections

--- inc/customizer/sections.php
+++ inc/customizer/sections.php
@@ -4,3 +4,30 @@
  *
  * @package _s
  */
+
+/**
+ * Register the section sections.
+ */
+function _s_customize_sections( $wp_customize ) {
+
+	// Register additional scripts section.
+	$wp_customize->add_section(
+		'_s_additional_scripts_section',
+		array(
+			'title'    => esc_html__( 'Additional Scripts', '_s' ),
+			'priority' => 10,
+			'panel'    => 'site-options',
+		)
+	);
+
+	// Register a footer section.
+	$wp_customize->add_section(
+		'_s_footer_section',
+		array(
+			'title'    => esc_html__( 'Footer Customizations', '_s' ),
+			'priority' => 90,
+			'panel'    => 'site-options',
+		)
+	);
+}
+add_action( 'customize_register', '_s_customize_sections' );
