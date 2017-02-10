b056dcbf71fb0f879a948e658a2d0914fa597e17
Greg Rickaby <gregrickaby@gmail.com>
2017-02-10 14:40:03 -0600

    clean up customizer.php and get things working

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -17,21 +17,41 @@ function _s_include_custom_controls() {
 add_action( 'customize_register', '_s_include_custom_controls', -999 );
 
 /**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ * Theme customizer related scripts.
  */
-function _s_customize_preview_js() {
-	wp_enqueue_script( 'tiny_mce' );
-	wp_enqueue_script( '_s-customize-tinymce', get_template_directory_uri() . '/inc/customizer/assets/scripts/tinymce.js', array( 'jquery' ), '1.0.0', true );
-	wp_enqueue_script( '_s-customizer', get_template_directory_uri() . '/inc/customizer/assets/scripts/customizer.js', array( 'customize-preview' ), '20151215', true );
+function _s_customize_scripts() {
+	wp_enqueue_script( '_s-customize-livepreview', get_template_directory_uri() . '/inc/customizer/assets/scripts/livepreview.js', array( 'jquery', 'customize-preview' ), '1.0.0', true );
 }
-add_action( 'customize_preview_init', '_s_customize_preview_js' );
+add_action( 'customize_preview_init', '_s_customize_scripts' );
+
+/**
+ * Add support for the fancy new edit icons.
+ *
+ * @link https://make.wordpress.org/core/2016/02/16/selective-refresh-in-the-customizer/
+ */
+function _s_selective_refresh_support( $wp_customize ) {
+
+	// The <div> classname to append edit icon too.
+	$settings = array(
+		'blogname'          => '.site-title a',
+		'blogdescription'   => '.site-description',
+		'_s_copyright_text' => '.site-info',
+	);
+
+	// Loop through, and add selector partials.
+	foreach ( (array) $settings as $setting => $selector ) {
+		$args = array( 'selector' => $selector );
+		$wp_customize->selective_refresh->add_partial( $setting, $args );
+	}
+}
+add_action( 'customize_register', '_s_selective_refresh_support' );
 
 /**
  * Add live preview support via postMessage.
  *
+ * Note: You will need to hook this up via livepreview.js
+ *
  * @link https://codex.wordpress.org/Theme_Customization_API#Part_3:_Configure_Live_Preview_.28Optional.29
- * @param object $wp_customize An instance of WP_Customize_Manager class.
- * @author Greg Rickaby
  */
 function _s_live_preview_support( $wp_customize ) {
 
@@ -39,6 +59,8 @@ function _s_live_preview_support( $wp_customize ) {
 	$settings = array(
 		'blogname',
 		'blogdescription',
+		'header_textcolor',
+		'background_image',
 		'_s_copyright_text',
 	);
 
@@ -58,27 +80,3 @@ function _s_live_preview_support( $wp_customize ) {
 	}
 }
 add_action( 'customize_register', '_s_live_preview_support', 999 );
-
-/**
- * Add support for the fancy new edit icons.
- *
- * @link https://make.wordpress.org/core/2016/02/16/selective-refresh-in-the-customizer/
- * @param object $wp_customize An instance of WP_Customize_Manager class.
- * @author Greg Rickaby
- */
-function _s_selective_refresh_support( $wp_customize ) {
-
-	// The <div> classname to append edit icon too.
-	$settings = array(
-		'blogname'          => '.site-title a',
-		'blogdescription'   => '.site-description',
-		'_s_copyright_text' => '.site-info',
-	);
-
-	// Loop through, and add selector partials.
-	foreach ( (array) $settings as $setting => $selector ) {
-		$args = array( 'selector' => $selector );
-		$wp_customize->selective_refresh->add_partial( $setting, $args );
-	}
-}
-add_action( 'customize_register', '_s_selective_refresh_support' );
