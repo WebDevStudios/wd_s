e2300ed25fd68151a5a50713740c7033291b4eb9
Carrie Forde <carrie@carrieforde.com>
2017-03-24 14:17:03 -0700

    Add translation notes to gettext strings

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,11 +25,13 @@ if ( ! function_exists( '_s_posted_on' ) ) :
 		);
 
 		$posted_on = sprintf(
+			/* translators: the date the post was published */
 			esc_html_x( 'Posted on %s', 'post date', '_s' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
 		$byline = sprintf(
+			/* translators: the post author */
 			esc_html_x( 'by %s', 'post author', '_s' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
@@ -49,12 +51,14 @@ if ( ! function_exists( '_s_entry_footer' ) ) :
 			/* translators: used between list items, there is a space after the comma */
 			$categories_list = get_the_category_list( esc_html__( ', ', '_s' ) );
 			if ( $categories_list && _s_categorized_blog() ) {
+				/* translators: the post category */
 				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', '_s' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 			}
 
 			/* translators: used between list items, there is a space after the comma */
 			$tags_list = get_the_tag_list( '', esc_html__( ', ', '_s' ) );
 			if ( $tags_list ) {
+				/* translators: the post tags */
 				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', '_s' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 			}
 		}
@@ -369,8 +373,8 @@ function _s_get_social_network_links() {
 		if ( isset( $network_url ) && ! empty( $network_url ) ) : ?>
 			<li class="social-icon <?php echo esc_attr( $network ); ?>">
 				<a href="<?php echo esc_url( $network_url ); ?>">
-					<?php echo _s_get_svg( array( 'icon' => $network . '-square', 'title' => sprintf( __( 'Link to %s', '_s' ), ucwords( esc_html( $network ) ) ) ) ); // WPCS: XSS ok. ?>
-					<span class="screen-reader-text"><?php echo sprintf( __( 'Link to %s', '_s' ), ucwords( esc_html( $network ) ) ); // WPCS: XSS ok. ?></span>
+					<?php echo _s_get_svg( array( 'icon' => $network . '-square', 'title' => /* translators: the social network name */ sprintf( __( 'Link to %s', '_s' ), ucwords( esc_html( $network ) ) ) ) ); // WPCS: XSS ok. ?>
+					<span class="screen-reader-text"><?php echo /* translators: the social network name */ sprintf( __( 'Link to %s', '_s' ), ucwords( esc_html( $network ) ) ); // WPCS: XSS ok. ?></span>
 				</a>
 			</li><!-- .social-icon -->
 		<?php endif;
