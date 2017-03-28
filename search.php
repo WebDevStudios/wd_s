03f518f9e418ecc1ec7df1fbbb8e31e724b2017e
Carrie Forde <carrie@carrieforde.com>
2017-03-28 06:44:49 -0700

    Update PHPCS & theme linting; fixes #256 (#257)
    * Update / add exclusions to our PHPCS ruleset
    
    * Update tinymce.php file name to be same as class name
    
    * Add translation notes to gettext strings

--- search.php
+++ search.php
@@ -17,7 +17,7 @@ get_header(); ?>
 			if ( have_posts() ) : ?>
 
 				<header class="page-header">
-					<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+					<h1 class="page-title"><?php /* translators: the term(s) searched */ printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<?php
