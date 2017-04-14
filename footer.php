3567559324fb8957efe71a930eec607d50c84119
Greg Rickaby <gregrickaby@gmail.com>
2017-04-14 09:40:41 -0500

    remove tapbar 🏗

--- footer.php
+++ footer.php
@@ -24,7 +24,18 @@
 	</footer><!-- .site-footer -->
 </div><!-- #page -->
 
-<?php echo wp_kses_post( _s_get_mobile_navigation_menu() ); ?>
+<nav id="mobile-menu" class="mobile-nav-menu">
+	<button class="close-mobile-menu"><span class="screen-reader-text"><?php echo esc_html_e( 'Close menu', '_s' ); ?></span><?php echo _s_get_svg( array( 'icon' => 'close' ) ); // WPCS: XSS ok. ?></button>
+	<?php
+		wp_nav_menu( array(
+			'theme_location' => $mobile_menu,
+			'menu_id'        => 'primary-menu',
+			'menu_class'     => 'menu dropdown mobile-nav',
+			'link_before'    => '<span>',
+			'link_after'     => '</span>',
+		) );
+	?>
+</nav>
 
 <?php wp_footer(); ?>
 
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -320,34 +320,6 @@ function _s_get_social_share() {
 }
 
 /**
- * Output the mobile navigation
- */
-function _s_get_mobile_navigation_menu() {
-
-	// Figure out which menu we're pulling.
-	$mobile_menu = has_nav_menu( 'mobile' ) ? 'mobile' : 'primary';
-
-	// Start the markup.
-	ob_start();
-	?>
-
-	<nav id="mobile-menu" class="mobile-nav-menu">
-		<button class="close-mobile-menu"><span class="screen-reader-text"><?php echo esc_html_e( 'Close menu', '_s' ); ?></span><?php echo _s_get_svg( array( 'icon' => 'close' ) ); // WPCS: XSS ok. ?></button>
-		<?php
-			wp_nav_menu( array(
-				'theme_location' => $mobile_menu,
-				'menu_id'        => 'primary-menu',
-				'menu_class'     => 'menu dropdown mobile-nav',
-				'link_before'    => '<span>',
-				'link_after'     => '</span>',
-			) );
-		?>
-	</nav>
-	<?php
-	return ob_get_clean();
-}
-
-/**
  * Retrieve the social links saved in the customizer
  *
  * @return mixed HTML output of social links
