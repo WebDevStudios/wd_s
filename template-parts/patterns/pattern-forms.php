3c3b40986dff4a4caf260c6c01cdfb0ed18f773b
Carrie Forde <carrie@carrieforde.com>
2017-04-24 14:38:41 -0700

    Move  assignment above function to pass PHPCS

--- template-parts/patterns/pattern-forms.php
+++ template-parts/patterns/pattern-forms.php
@@ -11,10 +11,11 @@
 	<h2 class="pattern-heading"><?php esc_html_e( 'Forms', '_s' ); ?></h2>
 
 	<?php // Search form.
+	$echo = false; // set echo to false so the search form outputs correctly.
 	_s_display_pattern_section( array(
 		'title'       => 'Search Form',
 		'description' => 'Display the search form.',
 		'usage'       => '<?php get_search_form(); ?>',
-		'output'      => get_search_form( $echo = false ),
+		'output'      => get_search_form( $echo ),
 	) ); ?>
 </section>
