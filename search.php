6aadc5c167d4326b6c9352eaf7b0bc26e60a2268
Rich Edmunds <rich@webdevstudios.com>
2017-06-12 08:59:33 -0600

    Adjust indenting and spacing.

--- search.php
+++ search.php
@@ -9,43 +9,34 @@
 
 get_header(); ?>
 
-	<div class="wrap">
-		<section class="primary content-area">
-			<main id="main" class="site-main" role="main">
+<div class="wrap">
+	<section class="primary content-area">
+		<main id="main" class="site-main" role="main">
+		<?php
+		if ( have_posts() ) : ?>
+			<header class="page-header">
+				<h1 class="page-title"><?php /* translators: the term(s) searched */ printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+			</header><!-- .page-header -->
 
 			<?php
-			if ( have_posts() ) : ?>
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+				/**
+				 * Run the loop for the search to output the results.
+				 * If you want to overload this in a child theme then include a file
+				 * called content-search.php and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', 'search' );
+			endwhile;
 
-				<header class="page-header">
-					<h1 class="page-title"><?php /* translators: the term(s) searched */ printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-				</header><!-- .page-header -->
+			the_posts_navigation();
+		else :
+			get_template_part( 'template-parts/content', 'none' );
+		endif; ?>
+		</main><!-- #main -->
+	</section><!-- .primary -->
 
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
-
-					/**
-					 * Run the loop for the search to output the results.
-					 * If you want to overload this in a child theme then include a file
-					 * called content-search.php and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', 'search' );
-
-				endwhile;
-
-				the_posts_navigation();
-
-			else :
-
-				get_template_part( 'template-parts/content', 'none' );
-
-			endif; ?>
-
-			</main><!-- #main -->
-		</section><!-- .primary -->
-
-		<?php get_sidebar(); ?>
-
-	</div><!-- .wrap -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
 
 <?php get_footer(); ?>
