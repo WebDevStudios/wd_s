00b301e07fb2311936690a9d1694ab8d262c7cd7
Rich Edmunds <rich@webdevstudios.com>
2017-06-12 08:52:07 -0600

    Adjust indenting and spacing.

--- index.php
+++ index.php
@@ -14,46 +14,36 @@
 
 get_header(); ?>
 
-	<div class="wrap">
-		<div class="primary content-area">
-			<main id="main" class="site-main" role="main">
-
+<div class="wrap">
+	<div class="primary content-area">
+		<main id="main" class="site-main" role="main">
 			<?php
 			if ( have_posts() ) :
-
 				if ( is_home() && ! is_front_page() ) : ?>
 					<header>
 						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 					</header>
-
-				<?php
+					<?php
 				endif;
 
 				/* Start the Loop */
 				while ( have_posts() ) : the_post();
-
 					/*
 					 * Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
 					get_template_part( 'template-parts/content', get_post_format() );
-
 				endwhile;
 
 				the_posts_navigation();
-
 			else :
-
 				get_template_part( 'template-parts/content', 'none' );
-
 			endif; ?>
+		</main><!-- #main -->
+	</div><!-- .primary -->
 
-			</main><!-- #main -->
-		</div><!-- .primary -->
-
-		<?php get_sidebar(); ?>
-
-	</div><!-- .wrap -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
 
 <?php get_footer(); ?>
