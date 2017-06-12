36c4086a019d14e3a1d726496e8c8f6d08913abf
Rich Edmunds <rich@webdevstudios.com>
2017-06-12 08:38:52 -0600

    Adjust indenting.

--- archive.php
+++ archive.php
@@ -9,46 +9,46 @@
 
 get_header(); ?>
 
-	<div class="wrap">
-		<div class="primary content-area">
-			<main id="main" class="site-main" role="main">
+<div class="wrap">
+	<div class="primary content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
 
 			<?php
-			if ( have_posts() ) : ?>
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
 
-				<header class="page-header">
-					<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="archive-description">', '</div>' );
-					?>
-				</header><!-- .page-header -->
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_format() );
 
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
+			endwhile;
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
+			the_posts_navigation();
 
-				endwhile;
+		else :
 
-				the_posts_navigation();
+			get_template_part( 'template-parts/content', 'none' );
 
-			else :
+		endif; ?>
 
-				get_template_part( 'template-parts/content', 'none' );
+		</main><!-- #main -->
+	</div><!-- .primary -->
 
-			endif; ?>
+	<?php get_sidebar(); ?>
 
-			</main><!-- #main -->
-		</div><!-- .primary -->
-
-		<?php get_sidebar(); ?>
-
-	</div><!-- .wrap -->
+</div><!-- .wrap -->
 
 <?php get_footer(); ?>
