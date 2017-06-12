56887b5a5524da30422578a91bb3e85d7a8e43ac
Rich Edmunds <rich@webdevstudios.com>
2017-06-12 08:53:03 -0600

    Adjust indenting.

--- page.php
+++ page.php
@@ -14,28 +14,23 @@
 
 get_header(); ?>
 
-	<div class="wrap">
-		<div class="primary content-area">
-			<main id="main" class="site-main" role="main">
+<div class="wrap">
+	<div class="primary content-area">
+		<main id="main" class="site-main" role="main">
+			<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content', 'page' );
 
-				<?php
-				while ( have_posts() ) : the_post();
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			endwhile; // End of the loop.
+			?>
+		</main><!-- #main -->
+	</div><!-- .primary -->
 
-					get_template_part( 'template-parts/content', 'page' );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-
-				endwhile; // End of the loop.
-				?>
-
-			</main><!-- #main -->
-		</div><!-- .primary -->
-
-		<?php get_sidebar(); ?>
-
-	</div><!-- .wrap -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
 
 <?php get_footer(); ?>
