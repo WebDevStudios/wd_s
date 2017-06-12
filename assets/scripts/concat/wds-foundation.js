265cc6c3cbca8acaa9d27c53fb55e077f321c721
Corey Collins <coreymcollins@gmail.com>
2017-06-12 13:30:05 -0500

    Initializes Foundation in a JS file

---
+++ assets/scripts/concat/wds-foundation.js
@@ -0,0 +1,11 @@
+/**
+ * File: wds-foundation.js
+ *
+ * We must simply initialize Foundation for Foundation JS to do its thing.
+ */
+window.wdsFoundation = {};
+( function( window, $, app ) {
+
+	$( document ).foundation();
+
+})( window, jQuery, window.wdsFoundation );
