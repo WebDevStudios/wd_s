352707d859670caaaa8f058fb5b8a1f204369c10
Rich Edmunds <rich@webdevstudios.com>
2017-06-12 09:01:02 -0600

    Adjust indenting and spacing.

--- single.php
+++ single.php
@@ -9,30 +9,25 @@
 
 get_header(); ?>
 
-	<div class="wrap">
-		<div class="primary content-area">
-			<main id="main" class="site-main" role="main">
+<div class="wrap">
+	<div class="primary content-area">
+		<main id="main" class="site-main" role="main">
+		<?php
+		while ( have_posts() ) : the_post();
+			get_template_part( 'template-parts/content', get_post_format() );
 
-			<?php
-			while ( have_posts() ) : the_post();
+			the_post_navigation();
 
-				get_template_part( 'template-parts/content', get_post_format() );
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+		endwhile; // End of the loop.
+		?>
+		</main><!-- #main -->
+	</div><!-- .primary -->
 
-				the_post_navigation();
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-			</main><!-- #main -->
-		</div><!-- .primary -->
-
-		<?php get_sidebar(); ?>
-
-	</div><!-- .wrap -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
 
 <?php get_footer(); ?>
