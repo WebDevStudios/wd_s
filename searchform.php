542d84ccaea89d35997e946bb44c4a1d2b66dd7d
Greg Rickaby <gregrickaby@gmail.com>
2017-06-13 14:14:29 -0500

    use correct escaping function

--- searchform.php
+++ searchform.php
@@ -10,6 +10,6 @@
   <span class="input-group-label show-for-sr"><?php esc_html_e( 'To search this site, enter a search term', '_s' ) ?></span>
   <input class="input-group-field"id="search-field" type="text" name="s" value="<?php echo get_search_query() ?>" aria-required="false" autocomplete="off" placeholder="<?php echo esc_attr_x( 'Search', '_s' ) ?>" />
   <div class="input-group-button">
-    <input type="submit" class="button" value="<?php esc_html_e( 'Submit', '_s' ); ?>">
+    <input type="submit" class="button" value="<?php esc_attr_x( 'Submit', '_s' ); ?>">
   </div>
 </form>
