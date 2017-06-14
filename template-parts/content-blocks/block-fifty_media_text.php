2c66d2ff55db23e5b760dc7c7c7c9b3725dbd32e
Greg Rickaby <gregrickaby@gmail.com>
2017-06-14 13:15:02 -0500

    move .row container
    in order to support full-width background options (yes, it does happen) move the  .row out of the block options function

--- template-parts/content-blocks/block-fifty_media_text.php
+++ template-parts/content-blocks/block-fifty_media_text.php
@@ -11,14 +11,16 @@ $image_data = get_sub_field( 'media_left' );
 // Start a <container> with a possible media background.
 _s_display_block_options( array(
 	'container' => 'section', // Any HTML5 container: section, div, etc...
-	'class'     => 'row content-block fifty-media-text', // The class of the container.
+	'class'     => 'content-block fifty-media-text', // The class of the container.
 ) );
 ?>
-	<div class="column small-12 medium-6 fifty-media-wrap">
-		<img class="fifty-media-image" src="<?php echo esc_url( $image_data['sizes']['medium'] );  ?>" alt="<?php echo esc_html( $image_data['alt'] ); ?>">
-	</div><!-- .fifty-media-wrap -->
+	<div class="row">
+		<div class="column small-12 medium-6 fifty-media-wrap">
+			<img class="fifty-media-image" src="<?php echo esc_url( $image_data['sizes']['medium'] );  ?>" alt="<?php echo esc_html( $image_data['alt'] ); ?>">
+		</div><!-- .fifty-media-wrap -->
 
-	<div class="column small-12 medium-6 fifty-text-wrap">
-		<?php echo force_balance_tags( get_sub_field( 'content_right' ) ); // WPCS XSS OK. ?>
-	</div><!-- .fifty-text-wrap -->
+		<div class="column small-12 medium-6 fifty-text-wrap">
+			<?php echo force_balance_tags( get_sub_field( 'content_right' ) ); // WPCS XSS OK. ?>
+		</div><!-- .fifty-text-wrap -->
+	</div><!-- .row -->
 </section><!-- .fifty-media-text -->
--- template-parts/content-blocks/block-generic_content.php
+++ template-parts/content-blocks/block-generic_content.php
@@ -8,10 +8,10 @@
 // Start a <container> with a possible media background.
 _s_display_block_options( array(
 	'container' => 'section', // Any HTML5 container: section, div, etc...
-	'class'     => 'row content-block generic-content', // The class of the container.
+	'class'     => 'content-block generic-content', // The class of the container.
 ) );
 ?>
-	<div class="column">
+	<div class="row">
 		<?php echo force_balance_tags( get_sub_field( 'content' ) ); // WP XSS OK. ?>
 	</div>
 </section><!-- .generic-content -->
