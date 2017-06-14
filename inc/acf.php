3710852cc7aa30deb3065cb31dec09aa8c149481
Greg Rickaby <gregrickaby@gmail.com>
2017-06-14 11:41:21 -0500

    add _s_display_block_options
    it’s great to have block options available by default…

--- inc/acf.php
+++ inc/acf.php
@@ -18,3 +18,48 @@ function _s_display_content_blocks() {
 		wp_reset_postdata();
 	endif;
 }
+
+/**
+ * Associate the possible block options with the appropriate section.
+ *
+ * @param  array $args Possible arguments.
+ */
+function _s_display_block_options( $args = array() ) {
+
+	// Setup defaults.
+	$defaults = array(
+		'background_type' => get_sub_field( 'background_type' ),
+		'font_color'      => get_sub_field( 'font_color' ),
+		'container'       => 'section',
+		'class'           => 'content-block',
+	);
+
+	// Parse args.
+	$args = wp_parse_args( $args, $defaults );
+
+	$inline_style = '';
+
+	if ( '' !== $args['font_color'] ) {
+		$inline_style = 'color: ' . $args['font_color'] . ';';
+	}
+
+	// Only try to get the rest of the settings if the background type is set to anything.
+	if ( $args['background_type'] ) {
+		if ( 'color' === $args['background_type']['value'] ) {
+			$background_color = get_sub_field( 'background_color' );
+			$inline_style .= 'background-color: ' . $background_color . ';';
+		}
+
+		if ( 'image' === $args['background_type']['value'] ) {
+			$background_image = get_sub_field( 'background_image' );
+			$inline_style .= 'background-image: url(' . esc_url( $background_image['sizes']['full-width'] ) . ');';
+			$args['class'] .= ' image-as-background';
+		}
+
+		if ( 'none' === $args['background_type']['value'] ) {
+			$args['class'] .= ' no-background';
+		}
+	}
+
+	printf( '<%s class="%s" style="%s">', esc_html( $args['container'] ), esc_attr( $args['class'] ), esc_attr( $inline_style ) );
+}
