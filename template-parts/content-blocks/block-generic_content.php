31c3e65ed8101ae5dc5f2df2b1efa703c14c1580
Greg Rickaby <gregrickaby@gmail.com>
2017-06-14 12:21:15 -0500

    update generic content markup

--- template-parts/content-blocks/block-generic_content.php
+++ template-parts/content-blocks/block-generic_content.php
@@ -5,15 +5,13 @@
  * @package _s
  */
 
-// Set up fields.
-$title = get_sub_field( 'title' );
-$content = get_sub_field( 'content' );
-
+// Start a <container> with a possible media background.
+_s_display_block_options( array(
+	'container' => 'section', // Any HTML5 container: section, div, etc...
+	'class'     => 'row generic-content', // The class of the container.
+) );
 ?>
-<section class="generic-content">
-	<?php if ( $title ) : ?>
-		<p class="generic-content-title"><?php echo esc_html( $title ); ?>
-	<?php endif; ?>
-
-	<?php echo force_balance_tags( $content ); // WP XSS OK. ?>
-</section><!-- .hero-area -->
+	<div class="column">
+		<?php echo force_balance_tags( get_sub_field( 'content' ) ); // WP XSS OK. ?>
+	</div>
+</section><!-- .generic-content -->
