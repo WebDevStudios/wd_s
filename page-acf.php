908b1e4fa2cb732c83ff6986d78141c211ba3b39
Greg Rickaby <gregrickaby@gmail.com>
2017-07-14 14:34:34 -0500

    add missing page-acf.php

---
+++ page-acf.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Page w/ ACF
+ *
+ * The template for displaying pages with ACF components.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _s
+ */
+
+get_header(); ?>
+
+	<div class="wrap">
+		<div class="primary content-area">
+			<main id="main" class="site-main">
+			<?php _s_display_content_blocks(); ?>
+			</main><!-- #main -->
+		</div><!-- .primary -->
+	</div><!-- .wrap -->
+
+<?php get_footer(); ?>
