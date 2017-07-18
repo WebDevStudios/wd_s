8cb23b094e1f9d683154b9b9ce8cc1b5b7758517
Kellen Mace <kellen@webdevstudios.com>
2017-07-18 08:15:09 -0400

    Move markup to display social sharing icons from a template tag to a template part.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -277,49 +277,6 @@ function _s_display_copyright_text() {
 }
 
 /**
- * Display social sharing icons.
- */
-function _s_display_social_icons() {
-	?>
-	<div class="social-share">
-		<h5 class="social-share-title"><?php esc_html_e( 'Share This', '_s' ); ?></h5>
-		<ul class="social-icons menu menu-horizontal">
-			<li class="social-icon">
-				<a href="<?php echo esc_url( _s_get_twitter_share_url() ); ?>" onclick="window.open(this.href, 'targetWindow', 'toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=yes, top=150, left=0, width=600, height=300' ); return false;">
-					<?php echo _s_get_svg( array( // WPCS: XSS ok.
-						'icon'  => 'twitter-square',
-						'title' => 'Twitter',
-						'desc'  => esc_html__( 'Share on Twitter', '_s' ),
-					) ); ?>
-					<span class="screen-reader-text"><?php esc_html_e( 'Share on Twitter', '_s' ); ?></span>
-				</a>
-			</li>
-			<li class="social-icon">
-				<a href="<?php echo esc_url( _s_get_facebook_share_url() ); ?>" onclick="window.open(this.href, 'targetWindow', 'toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=yes, top=150, left=0, width=600, height=300' ); return false;">
-					<?php echo _s_get_svg( array( // WPCS: XSS ok.
-						'icon'  => 'facebook-square',
-						'title' => 'Facebook',
-						'desc'  => esc_html__( 'Share on Facebook', '_s' ),
-					) ); ?>
-					<span class="screen-reader-text"><?php esc_html_e( 'Share on Facebook', '_s' ); ?></span>
-				</a>
-			</li>
-			<li class="social-icon">
-				<a href="<?php echo esc_url( _s_get_linkedin_share_url() ); ?>" onclick="window.open(this.href, 'targetWindow', 'toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=yes, top=150, left=0, width=475, height=505' ); return false;">
-					<?php echo _s_get_svg( array( // WPCS: XSS ok.
-						'icon'  => 'linkedin-square',
-						'title' => 'LinkedIn',
-						'desc'  => esc_html__( 'Share on LinkedIn', '_s' ),
-					) ); ?>
-					<span class="screen-reader-text"><?php esc_html_e( 'Share on LinkedIn', '_s' ); ?></span>
-				</a>
-			</li>
-		</ul>
-	</div><!-- .social-share -->
-	<?php
-}
-
-/**
  * Get the Twitter social sharing URL for the current page.
  *
  * @return string The URL.
