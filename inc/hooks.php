635283e142e6022e8f1a6b8dbabaab9e05848379
Carrie Forde <carrie@carrieforde.com>
2017-07-19 17:57:52 -0700

    Fixes template name

--- inc/hooks.php
+++ inc/hooks.php
@@ -58,7 +58,7 @@ function _s_body_classes( $classes ) {
 	}
 
 	// Add a `has-sidebar` class if we're using the sidebar template.
-	if ( is_page_template( 'template-right-sidebar.php' ) ) {
+	if ( is_page_template( 'template-sidebar-right.php' ) ) {
 		$classes[] = 'has-sidebar';
 	}
 
