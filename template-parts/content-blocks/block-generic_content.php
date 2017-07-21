748b9fd1faa62de1b49891b98e0d77b7de47b515
Shannon MacMillan <shannon@webdevstudios.com>
2017-07-21 11:31:14 -0400

    Update generic content template part

--- template-parts/content-blocks/block-generic_content.php
+++ template-parts/content-blocks/block-generic_content.php
@@ -6,13 +6,22 @@
  */
 
 // Set up fields.
+$title = get_sub_field( 'title' );
 $content = get_sub_field( 'content' );
 
-// Start a <container> with a possible background media.
-_s_display_block_options( array(
-	'container' => 'section', // Any HTML5 container: section, div, etc...
-	'class'     => 'content-block generic-content', // The class of the container.
-) );
+// Start a <container> with possible block options.
+echo _s_display_block_options( // WPCS: XSS OK.
+	array(
+		'container' => 'section', // Any HTML5 container: section, div, etc...
+		'class'     => 'generic-content', // Container class.
+	)
+);
 ?>
-	<?php echo force_balance_tags( $content ); // WP XSS OK. ?>
+	<div class="wrap">
+		<?php if ( $title ) : ?>
+			<h2 class="generic-content-title"><?php echo esc_html( $title ); ?></h2>
+		<?php endif; ?>
+
+		<?php echo force_balance_tags( $content ); // WP XSS OK. ?>
+	</div>
 </section><!-- .generic-content -->
