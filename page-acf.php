38190277a7474d7825b118406d3d89c32642f67f
Will Schmierer <will@webdevstudios.com>
2017-07-21 15:27:25 -0400

    remove full width from main

--- page-acf.php
+++ page-acf.php
@@ -12,7 +12,7 @@
 get_header(); ?>
 
 	<div class="primary content-area">
-		<main id="main" class="site-main full-width">
+		<main id="main" class="site-main">
 		<?php _s_display_content_blocks(); ?>
 		</main><!-- #main -->
 	</div><!-- .primary -->
