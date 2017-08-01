eebf7679936dafae4755b581be353c8ea0743839
Greg Rickaby <gregrickaby@gmail.com>
2017-08-01 13:30:58 -0500

    refactor call to action block markup

--- template-parts/content-blocks/block-cta.php
+++ template-parts/content-blocks/block-cta.php
@@ -6,39 +6,35 @@
  */
 
 // Set up fields.
-$headline     = get_sub_field( 'headline' );
-$sub_heading  = get_sub_field( 'sub_heading' );
-$button_url   = get_sub_field( 'button_url' );
-$button_text  = get_sub_field( 'button_text' );
-
-// Set up our animation class for the wrap.
+$title = get_sub_field( 'title' );
+$text = get_sub_field( 'text' );
+$button_url = get_sub_field( 'button_url' );
+$button_text = get_sub_field( 'button_text' );
 $animation_class = _s_get_animation_class();
 
 // Start a <container> with possible block options.
-echo _s_display_block_options( // WPCS: XSS OK.
+_s_display_block_options(
 	array(
 		'container' => 'section', // Any HTML5 container: section, div, etc...
-		'class'     => 'content-block container cta-block background-silver', // Container class.
+		'class'     => 'content-block container cta-block', // Container class.
 	)
 );
 ?>
 	<div class="row <?php echo esc_attr( $animation_class ) ?>">
 		<div class="col col-l-8">
-			<?php if ( $headline) : ?>
-				<h2 class="cta-headline color-white"><?php echo esc_html( $headline ); ?></h2>
+			<?php if ( $title ) : ?>
+				<h3 class="cta-title"><?php echo esc_html( $title ); ?></h3>
 			<?php endif; ?>
 
-			<?php if ( $sub_heading ) : ?>
-				<p class="cta-sub-heading color-white"><?php echo esc_html( $sub_heading ); ?></p>
+			<?php if ( $text ) : ?>
+				<h4 class="cta-text"><?php echo esc_html( $text ); ?></h4>
 			<?php endif; ?>
 		</div>
 
 		<div class="col col-l-4">
 			<?php if ( $button_url ) : ?>
-
-				<a class="cta button" href="<?php echo esc_url( $button_url ); ?>"><?php echo esc_html( $button_text ); ?></a>
-
+				<button type="button" class="button cta-button" onclick="location.href='<?php echo esc_url( $button_url ); ?>'"><?php echo esc_html( $button_text ); ?></button>
 			<?php endif; ?>
 		</div>
-	</div>
+	</div><!-- .row -->
 </section><!-- .cta-block -->
