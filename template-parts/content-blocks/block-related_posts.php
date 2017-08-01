590c4c240a0b76a4b9e242159eabfa204e9148c9
Greg Rickaby <gregrickaby@gmail.com>
2017-08-01 13:00:15 -0500

    refactor related posts block to match recent posts and use card

--- template-parts/content-blocks/block-related_posts.php
+++ template-parts/content-blocks/block-related_posts.php
@@ -1,78 +1,56 @@
 <?php
 /**
- * The template used for displaying related posts in a carousel.
+ * The template used for displaying related posts.
  *
  * @package _s
  */
 
 // Set up fields.
 $title = get_sub_field( 'title' );
-$posts = get_sub_field( 'related' );
+$related_posts = get_sub_field( 'related_posts' );
 
-if ( $posts ) : ?>
+// Display section if we have any posts.
+if ( $related_posts ) :
 
-	<section class="content-block related-posts">
+	// Start a <container> with possible block options.
+	_s_display_block_options(
+		array(
+			'container' => 'section', // Any HTML5 container: section, div, etc...
+			'class'     => 'content-block container related-posts', // Container class.
+		)
+	);
+
+	?>
+
+	<div class="row">
 
 		<?php if ( $title ) : ?>
-		<h2 class="related-posts-title"><?php echo esc_html( $title ); ?></h2>
+		<h2 class="content-block-title"><?php echo esc_html( $title ); ?></h2>
 		<?php endif; ?>
 
-		<?php foreach ( $posts as $key => $post ) :
+		<?php
+		// Loop through recent posts.
+		foreach ( $related_posts as $key => $post ) :
 
+			// Convert post object to post data.
 			setup_postdata( $post );
 
-			// Get the post data.
-			$post_title = get_the_title();
-			$post_excerpt = get_the_excerpt();
-			$post_link = get_the_permalink();
-			$post_image = get_the_post_thumbnail( get_the_ID() );
-			$post_categories = get_the_category();
-			$first_category = '';
-
-			if ( count( $post_categories ) > 0 ) {
-				$first_category = $post_categories[0]->name;
-			}
-
-			// Trim excerpt down to 170 characters.
-			if ( 170 < strlen( $post_excerpt ) ) {
-				$post_excerpt = mb_substr( strip_tags( $post_excerpt ), 0, 170 ) . esc_html__( '&hellip;', '_s' );
-			}
-
-		?>
-
-			<div class="post">
-
-				<div class="post-content-wrap">
-
-					<?php if ( $first_category ) : ?>
-						<span class="category-badge"><?php echo esc_html( $first_category ); ?></span>
-					<?php endif; ?>
-
-					<h3 class="post-title"><?php echo esc_html( $post_title ); ?></h3>
-
-					<p class="post-excerpt">
-						<?php echo esc_html( $post_excerpt ); ?>
-					</p>
-
-					<a href="<?php echo esc_url( $post_link ); ?>" class="button button-orange read-more">
-						<?php esc_html_e( 'Read More', '_s' ); ?>
-						<span class="screen-reader-text"> <?php echo esc_html( $post_title ); ?></span>
-					</a>
-				</div>
-
-				<?php
-
-				if ( has_post_thumbnail() ) {
-					the_post_thumbnail();
-				}
-
-				?>
-
-			</div>
-
-		<?php endforeach; ?>
-		<?php wp_reset_postdata(); ?>
-
-	</section><!-- .related-posts -->
-
+			// Display a card.
+			_s_display_card( array(
+					'title' => get_the_title(),
+					'image' => _s_get_post_image_url( 'medium' ),
+					'text'  => _s_get_the_excerpt( array(
+							'length' => 20,
+							'more'   => '...',
+						)
+					),
+					'url'   => get_the_permalink(),
+					'class' => 'col col-m-6 col-l-4',
+				)
+			);
+		endforeach;
+		wp_reset_postdata();
+	?>
+	</div><!-- .row -->
+</section><!-- .recent-posts -->
 <?php endif; ?>
