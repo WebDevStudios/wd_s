ba5c8885c8762a79c9b8c2258cdab0a58dbdc589
Greg Rickaby <gregrickaby@gmail.com>
2017-08-10 14:50:33 -0500

    add new svg params to scaffolding

--- template-parts/scaffolding/scaffolding-icons.php
+++ template-parts/scaffolding/scaffolding-icons.php
@@ -11,7 +11,8 @@
 
 	<h2 class="scaffolding-heading"><?php esc_html_e( 'Icons', '_s' ); ?></h2>
 
-	<?php // SVG
+	<?php 
+	// SVG Icon.
 	_s_display_scaffolding_section( array(
 		'title'       => 'SVG',
 		'description' => 'Display inline SVGs.',
@@ -20,9 +21,12 @@
 			'$args' => '(required) Configuration arguments.',
 		),
 		'arguments'    => array(
-			'icon'  => '(required) The SVG icon file name. Default none',
-			'title' => '(optional) The title of the icon. Default: none',
-			'desc'  => '(optional) The description of the icon. Default: none',
+			'icon'   => '(required) The SVG icon file name. Default none',
+			'title'  => '(optional) The title of the icon. Default: none',
+			'desc'   => '(optional) The description of the icon. Default: none',
+			'fill'   => '(optional) The fill color of the icon. Default: none',
+			'height' => '(optional) The height of the icon. Default: none',
+			'width'  => '(optional) The width of the icon. Default: none',
 		),
 		'output'       => _s_display_svg( array( 'icon' => 'facebook-square' ) ),
 	) ); ?>
