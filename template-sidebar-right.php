95e9a0e169b0ef72baad540cf64ad6769eeaa64f
jomurgel <dev@jomurgel.com>
2017-09-18 12:59:41 -0600

    adds sidebar helper container to allow for proper width

--- template-sidebar-right.php
+++ template-sidebar-right.php
@@ -9,27 +9,30 @@
  * @package _s
  */
 
+
 get_header(); ?>
 
-	<div class="primary content-area col-l-8">
-		<main id="main" class="site-main">
+	<div class="sidebar-adjustment row">
+		<div class="primary content-area col-l-8">
+			<main id="main" class="site-main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+				<?php
+				while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+					get_template_part( 'template-parts/content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-			endwhile; // End of the loop.
-			?>
+				endwhile; // End of the loop.
+				?>
 
-		</main><!-- #main -->
-	</div><!-- .primary -->
+			</main><!-- #main -->
+		</div><!-- .primary -->
 
-	<?php get_sidebar(); ?>
+		<?php get_sidebar(); ?>
+	</div><!-- .sidebar-adjustment -->
 
 <?php get_footer(); ?>
