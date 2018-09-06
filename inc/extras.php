b83e7dd515928455ed2615f15d6d3904d85850fa
Muhammad Haris <isharis@gmail.com>
2018-09-07 04:22:38 +0500

    Defined current year variable.

--- inc/extras.php
+++ inc/extras.php
@@ -240,23 +240,26 @@ function _s_display_customizer_footer_scripts() {
  *
  * @author Haris Zulfiqar
  * @param array $atts {.
- * @type string $starting_year Optional. Define starting year to show starting year and current year e.g. 2015 - 2018
+ * @type string $starting_year Optional. Define starting year to show starting year and current year e.g. 2015 - 2018.
  * @type string $separator Optional. Separator between starting year and current year.
  * }
  * @return string
  */
 function _s_copyright_year( $atts ) {
+
 	// Setup defaults.
 	$args = shortcode_atts( array(
 		'starting_year' => '',
 		'separator'     => ' - ',
 	), $atts );
 
+	$current_year = date( 'Y' );
+
 	// Return current year if starting year is empty.
 	if ( ! $args['starting_year'] ) {
-		return date( 'Y' );
+		return $current_year;
 	}
 
-	return $args['starting_year'] . $args['separator'] . date( 'Y' );
+	return esc_html( $args['starting_year'] . $args['separator'] . $current_year );
 }
 add_shortcode( '_s_copyright_year', '_s_copyright_year', 15 );
