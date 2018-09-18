0a7bf255c703478570428b5909fccf13614edbc7
Haris Zulfiqar <mharis@users.noreply.github.com>
2018-09-18 22:20:47 +0500

    #378 copyright year shortcode (#379)
    * Added copyright year shortcode
    
    * Added support for shortcodes within copyright text
    
    * Defined current year variable.

--- inc/extras.php
+++ inc/extras.php
@@ -233,3 +233,33 @@ function _s_display_customizer_footer_scripts() {
 	// Otherwise, echo the scripts!
 	echo force_balance_tags( $scripts ); // WPCS XSS OK.
 }
+
+
+/**
+ * Shortcode to display copyright year.
+ *
+ * @author Haris Zulfiqar
+ * @param array $atts {.
+ * @type string $starting_year Optional. Define starting year to show starting year and current year e.g. 2015 - 2018.
+ * @type string $separator Optional. Separator between starting year and current year.
+ * }
+ * @return string
+ */
+function _s_copyright_year( $atts ) {
+
+	// Setup defaults.
+	$args = shortcode_atts( array(
+		'starting_year' => '',
+		'separator'     => ' - ',
+	), $atts );
+
+	$current_year = date( 'Y' );
+
+	// Return current year if starting year is empty.
+	if ( ! $args['starting_year'] ) {
+		return $current_year;
+	}
+
+	return esc_html( $args['starting_year'] . $args['separator'] . $current_year );
+}
+add_shortcode( '_s_copyright_year', '_s_copyright_year', 15 );
