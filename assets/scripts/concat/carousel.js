1f41d49abf290f388ca31658305548c8d8f76d33
Greg Rickaby <gregrickaby@gmail.com>
2018-09-20 10:55:11 -0500

    refactor carousel

--- assets/scripts/concat/carousel.js
+++ assets/scripts/concat/carousel.js
@@ -3,111 +3,115 @@
  *
  * Deal with the Slick carousel.
  */
-window.wdsCarousel = {};
-( function( window, $, app ) {
 
-	// Constructor.
-	app.init = function() {
-		app.cache();
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-		if ( app.meetsRequirements() ) {
-			app.bindEvents();
+// App Object
+const app = {};
+
+// Constructor.
+app.init = function() {
+	app.cache();
+
+	if ( app.meetsRequirements() ) {
+		app.bindEvents();
+	}
+};
+
+// Cache all the things.
+app.cache = function() {
+	app.$c = {
+		window: $( window ),
+		theCarousel: $( '.carousel' )
+	};
+};
+
+// Combine all events.
+app.bindEvents = function() {
+	app.$c.window.on( 'load', app.doSlick );
+	app.$c.window.on( 'load', app.doFirstAnimation );
+};
+
+// Do we meet the requirements?
+app.meetsRequirements = function() {
+	return app.$c.theCarousel.length;
+};
+
+// Animate the first slide on window load.
+app.doFirstAnimation = function() {
+
+	// Get the first slide content area and animation attribute.
+	let firstSlide = app.$c.theCarousel.find( '[data-slick-index=0]' ),
+		firstSlideContent = firstSlide.find( '.slide-content' ),
+		firstAnimation = firstSlideContent.attr( 'data-animation' );
+
+	// Add the animation class to the first slide.
+	firstSlideContent.addClass( firstAnimation );
+};
+
+// Animate the slide content.
+app.doAnimation = function() {
+	let slides = $( '.slide' ),
+		activeSlide = $( '.slick-current' ),
+		activeContent = activeSlide.find( '.slide-content' ),
+
+		// This is a string like so: 'animated someCssClass'.
+		animationClass = activeContent.attr( 'data-animation' ),
+		splitAnimation = animationClass.split( ' ' ),
+
+		// This is the 'animated' class.
+		animationTrigger = splitAnimation[0];
+
+	// Go through each slide to see if we've already set animation classes.
+	slides.each( function() {
+		let slideContent = $( this ).find( '.slide-content' );
+
+		// If we've set animation classes on a slide, remove them.
+		if ( slideContent.hasClass( 'animated' ) ) {
+
+			// Get the last class, which is the animate.css class.
+			let lastClass = slideContent
+				.attr( 'class' )
+				.split( ' ' )
+				.pop();
+
+			// Remove both animation classes.
+			slideContent.removeClass( lastClass ).removeClass( animationTrigger );
 		}
-	};
+	} );
 
-	// Cache all the things.
-	app.cache = function() {
-		app.$c = {
-			window: $( window ),
-			theCarousel: $( '.carousel' )
-		};
-	};
+	// Add animation classes after slide is in view.
+	activeContent.addClass( animationClass );
+};
 
-	// Combine all events.
-	app.bindEvents = function() {
-		app.$c.window.on( 'load', app.doSlick );
-		app.$c.window.on( 'load', app.doFirstAnimation );
-	};
+// Allow background videos to autoplay.
+app.playBackgroundVideos = function() {
 
-	// Do we meet the requirements?
-	app.meetsRequirements = function() {
-		return app.$c.theCarousel.length;
-	};
+	// Get all the videos in our slides object.
+	$( 'video' ).each( function() {
 
-	// Animate the first slide on window load.
-	app.doFirstAnimation = function() {
+		// Let them autoplay. TODO: Possibly change this later to only play the visible slide video.
+		this.play();
+	} );
+};
 
-		// Get the first slide content area and animation attribute.
-		let firstSlide = app.$c.theCarousel.find( '[data-slick-index=0]' ),
-			firstSlideContent = firstSlide.find( '.slide-content' ),
-			firstAnimation = firstSlideContent.attr( 'data-animation' );
+// Kick off Slick.
+app.doSlick = function() {
+	app.$c.theCarousel.on( 'init', app.playBackgroundVideos );
 
-		// Add the animation class to the first slide.
-		firstSlideContent.addClass( firstAnimation );
-	};
+	app.$c.theCarousel.slick( {
+		autoplay: true,
+		autoplaySpeed: 5000,
+		arrows: true,
+		dots: true,
+		focusOnSelect: true,
+		waitForAnimate: true
+	} );
 
-	// Animate the slide content.
-	app.doAnimation = function() {
-		let slides = $( '.slide' ),
-			activeSlide = $( '.slick-current' ),
-			activeContent = activeSlide.find( '.slide-content' ),
+	app.$c.theCarousel.on( 'afterChange', app.doAnimation );
+};
 
-			// This is a string like so: 'animated someCssClass'.
-			animationClass = activeContent.attr( 'data-animation' ),
-			splitAnimation = animationClass.split( ' ' ),
-
-			// This is the 'animated' class.
-			animationTrigger = splitAnimation[0];
-
-		// Go through each slide to see if we've already set animation classes.
-		slides.each( function() {
-			let slideContent = $( this ).find( '.slide-content' );
-
-			// If we've set animation classes on a slide, remove them.
-			if ( slideContent.hasClass( 'animated' ) ) {
-
-				// Get the last class, which is the animate.css class.
-				let lastClass = slideContent
-					.attr( 'class' )
-					.split( ' ' )
-					.pop();
-
-				// Remove both animation classes.
-				slideContent.removeClass( lastClass ).removeClass( animationTrigger );
-			}
-		} );
-
-		// Add animation classes after slide is in view.
-		activeContent.addClass( animationClass );
-	};
-
-	// Allow background videos to autoplay.
-	app.playBackgroundVideos = function() {
-
-		// Get all the videos in our slides object.
-		$( 'video' ).each( function() {
-
-			// Let them autoplay. TODO: Possibly change this later to only play the visible slide video.
-			this.play();
-		} );
-	};
-
-	// Kick off Slick.
-	app.doSlick = function() {
-		app.$c.theCarousel.on( 'init', app.playBackgroundVideos );
-
-		app.$c.theCarousel.slick( {
-			autoplay: true,
-			autoplaySpeed: 5000,
-			arrows: true,
-			dots: true,
-			focusOnSelect: true,
-			waitForAnimate: true
-		} );
-
-		app.$c.theCarousel.on( 'afterChange', app.doAnimation );
-	};
-
-	// Engage!
-	$( app.init );
-} ( window, jQuery, window.wdsCarousel ) );
+// Engage!
+$( app.init );
