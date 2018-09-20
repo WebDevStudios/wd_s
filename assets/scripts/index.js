dbf50b14f83109d3e6ce5e7e70a148cbf1126905
Greg Rickaby <gregrickaby@gmail.com>
2018-09-20 12:55:22 -0500

    import all components

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,5 +2,12 @@ import 'normalize.css';
 import 'animate.css';
 import '../sass/style.scss';
 
-import './concat/header-button.js';
-import './concat/carousel.js';
+import './components/js-enabled.js';
+import './components/header-button.js';
+import './components/carousel.js';
+import './components/mobile-menu.js';
+import './components/modal.js';
+import './components/navigation-primary.js';
+import './components/off-canvas.js';
+import './components/skip-link-focus-fix.js';
+import './components/window-ready.js';
