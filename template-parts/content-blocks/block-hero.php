35ba5647f15ef538be2b2f8997ccf83c3185bf12
Corey Collins <coreymcollins@gmail.com>
2018-10-05 13:18:37 -0500

    Adds missing quotation marks around link in hero

--- template-parts/content-blocks/block-hero.php
+++ template-parts/content-blocks/block-hero.php
@@ -30,7 +30,7 @@ _s_display_block_options(
 		<?php endif; ?>
 
 		<?php if ( $button_text && $button_url ) : ?>
-			<a class="button button-hero" href=<?php echo esc_url( $button_url ); ?>><?php echo esc_html( $button_text ); ?></a>
+			<a class="button button-hero" href="<?php echo esc_url( $button_url ); ?>"><?php echo esc_html( $button_text ); ?></a>
 		<?php endif; ?>
 	</div><!-- .hero-content-->
 </section><!-- .hero -->
