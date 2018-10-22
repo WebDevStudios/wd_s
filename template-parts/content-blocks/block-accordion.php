ae410c412af0a97c49d47135df7de068d3dd555c
Corey Collins <coreymcollins@gmail.com>
2018-10-22 13:37:01 -0400

    Adjusts the accordion item IDs for multiple accordions on one page

--- template-parts/content-blocks/block-accordion.php
+++ template-parts/content-blocks/block-accordion.php
@@ -10,6 +10,7 @@ $title           = get_sub_field( 'title' );
 $text            = get_sub_field( 'text' );
 $accordion_items = get_sub_field( 'accordion_items' );
 $animation_class = _s_get_animation_class();
+$row_index       = get_row_index();
 
 // Start a <container> with possible block options.
 _s_display_block_options(
@@ -46,7 +47,7 @@ _s_display_block_options(
 							<h3 class="item-title"><?php echo esc_html( $item_title ); ?></h3>
 							<button class="item-toggle"><span>+</span></button>
 						</div><!-- .item-header-->
-						<div id="<?php echo esc_attr( 'accordion-item-' . $count ); ?>" class="item-content">
+						<div id="<?php echo esc_attr( 'accordion-' . intval( $row_index ) . '-item-' . $count ); ?>" class="item-content">
 							<div class="item-content-inner">
 								<?php echo wp_kses_post( $item_content ); ?>
 							</div><!-- .item-content-inner -->
