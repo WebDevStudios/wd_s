3957638b6119c22cb66861bb09d29882b87ffe11
jomurgel <dev@jomurgel.com>
2018-12-14 11:38:31 -0700

    wraps single post (sidebar)

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single posts.
  *
@@ -9,24 +10,24 @@
 
 get_header(); ?>
 
-	<div class="grid-wrapper">
+	<div class="display-flex grid-wrapper">
 		<main id="main" class="site-main">
 
 			<?php
-			while ( have_posts() ) :
-				the_post();
+		while (have_posts()) :
+			the_post();
 
-				get_template_part( 'template-parts/content', get_post_format() );
+		get_template_part('template-parts/content', get_post_format());
 
-				the_post_navigation();
+		the_post_navigation();
 
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+		if (comments_open() || get_comments_number()) :
+			comments_template();
+		endif;
 
-			endwhile; // End of the loop.
-			?>
+		endwhile; // End of the loop.
+		?>
 
 		</main><!-- #main -->
 
