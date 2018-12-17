3aa99cfebc82f495a72a474b62c1516a95b3c28f
jomurgel <dev@jomurgel.com>
2018-12-17 08:44:01 -0700

    wraps heading to allow for full-width elements

--- header.php
+++ header.php
@@ -28,42 +28,42 @@
 	<header class="site-header background-gallery">
 		<div class="display-flex container">
 
-		<div class="site-branding">
+			<div class="site-branding">
 
-			<?php the_custom_logo(); ?>
+				<?php the_custom_logo(); ?>
 
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php endif; ?>
+
+				<?php
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) :
+				?>
+					<p class="site-description"><?php echo esc_html( $description ); ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
+
+			<?php _s_display_header_button(); ?>
+
+			<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'mobile' ) ) : ?>
+				<button type="button" class="off-canvas-open" aria-expanded="false" aria-label="<?php esc_html_e( 'Open Menu', '_s' ); ?>">
+					<span class="hamburger"></span>
+				</button>
 			<?php endif; ?>
-
-			<?php
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) :
-			?>
-				<p class="site-description"><?php echo esc_html( $description ); ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<?php _s_display_header_button(); ?>
-
-		<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'mobile' ) ) : ?>
-			<button type="button" class="off-canvas-open" aria-expanded="false" aria-label="<?php esc_html_e( 'Open Menu', '_s' ); ?>">
-				<span class="hamburger"></span>
-			</button>
-		<?php endif; ?>
 		</div><!-- .container -->
 
-		<?php
-		wp_nav_menu( array(
-			'fallback_cb'     => false,
-			'theme_location'  => 'primary',
-			'menu_id'         => 'primary-menu',
-			'menu_class'      => 'menu dropdown',
-			'container'       => 'nav',
-			'container_class' => 'main-navigation',
-			'container_id'    => 'site-navigation',
-		) );
-		?>
+			<?php
+			wp_nav_menu( array(
+				'fallback_cb'     => false,
+				'theme_location'  => 'primary',
+				'menu_id'         => 'primary-menu',
+				'menu_class'      => 'menu dropdown',
+				'container'       => 'nav',
+				'container_class' => 'main-navigation background-alto',
+				'container_id'    => 'site-navigation',
+			) );
+			?>
 	</header><!-- .site-header-->
