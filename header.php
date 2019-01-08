8a10bf430d387c00985f9c50febe8aad107667fa
jomurgel <dev@jomurgel.com>
2019-01-08 14:13:45 -0700

    removes unused description

--- header.php
+++ header.php
@@ -37,13 +37,6 @@
 				<?php else : ?>
 					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php endif; ?>
-
-				<?php
-				$description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) :
-				?>
-					<p class="site-description"><?php echo esc_html( $description ); ?></p>
-				<?php endif; ?>
 			</div><!-- .site-branding -->
 
 			<?php _s_display_header_button(); ?>
