ff2c68bb29e26d60301f9909b4f25659b82223fc
Eric Fuller <info@glassmoonmedia.com>
2019-01-09 14:47:42 -0500

    Move current JS files into src/components folder.

---
+++ assets/scripts/src/index.js
@@ -0,0 +1,14 @@
+import './components/accordion';
+import './components/carousel';
+import './components/header-button';
+import './components/js-enabled';
+import './components/mobile-menu';
+import './components/modal';
+import './components/navigation-primary';
+import './components/off-canvas';
+import './components/skip-link-focus-fix';
+import './components/window-ready';
+
+if ( module.hot ) {
+	module.hot.accept();
+}
