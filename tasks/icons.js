ba08ad73c2b20d185ea478083b58452ee17f94ce
Eric Fuller <info@glassmoonmedia.com>
2019-01-14 13:10:00 -0500

    Refactor icon related tasks into their own file.

---
+++ tasks/icons.js
@@ -0,0 +1,56 @@
+const browserSync = require( 'browser-sync' );
+const cheerio = require( 'gulp-cheerio' );
+const gulp = require( 'gulp' );
+const del = require( 'del' );
+const plumber = require( 'gulp-plumber' );
+const rename = require( 'gulp-rename' );
+const svgmin = require( 'gulp-svgmin' );
+const svgstore = require( 'gulp-svgstore' );
+
+const paths = require( './config' ).paths;
+const handleErrors = require( './handle-errors' );
+
+/**
+ * Delete the svg-icons.svg before we minify, concat.
+ */
+gulp.task( 'clean:icons', () =>
+	del( [ 'assets/images/svg-icons.svg' ] )
+);
+
+/**
+ * Minify, concatenate, and clean SVG icons.
+ *
+ * https://www.npmjs.com/package/gulp-svgmin
+ * https://www.npmjs.com/package/gulp-svgstore
+ * https://www.npmjs.com/package/gulp-cheerio
+ */
+gulp.task( 'svg', [ 'clean:icons' ], () =>
+	gulp.src( paths.icons )
+
+	// Deal with errors.
+		.pipe( plumber( {'errorHandler': handleErrors} ) )
+
+		// Minify SVGs.
+		.pipe( svgmin() )
+
+		// Add a prefix to SVG IDs.
+		.pipe( rename( {'prefix': 'icon-'} ) )
+
+		// Combine all SVGs into a single <symbol>
+		.pipe( svgstore( {'inlineSvg': true} ) )
+
+		// Clean up the <symbol> by removing the following cruft...
+		.pipe( cheerio( {
+			'run': function( $, file ) {
+				$( 'svg' ).attr( 'style', 'display:none' );
+				$( '[fill]' ).removeAttr( 'fill' );
+				$( 'path' ).removeAttr( 'class' );
+				$( 'title' ).remove();
+			},
+			'parserOptions': {'xmlMode': true}
+		} ) )
+
+		// Save svg-icons.svg.
+		.pipe( gulp.dest( 'assets/images/' ) )
+		.pipe( browserSync.stream() )
+);
