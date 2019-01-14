a48ef1724230286dbf8db7af501b1cf578a2a1aa
Eric Fuller <info@glassmoonmedia.com>
2019-01-14 13:03:05 -0500

    Refactor out style tasks into their own file.

---
+++ tasks/styles.js
@@ -0,0 +1,115 @@
+const autoprefixer = require( 'autoprefixer' );
+const bourbon = require( 'bourbon' ).includePaths;
+const browserSync = require( 'browser-sync' );
+const cssnano = require( 'gulp-cssnano' );
+const del = require( 'del' );
+const gulp = require( 'gulp' );
+const mqpacker = require( 'css-mqpacker' );
+const neat = require( 'bourbon-neat' ).includePaths;
+const plumber = require( 'gulp-plumber' );
+const postcss = require( 'gulp-postcss' );
+const rename = require( 'gulp-rename' );
+const sass = require( 'gulp-sass' );
+const sassdoc = require( 'sassdoc' );
+const sassLint = require( 'gulp-sass-lint' );
+const sourcemaps = require( 'gulp-sourcemaps' );
+
+const paths = require( './config' ).paths;
+const handleErrors = require( './handle-errors' );
+
+/**
+ * Delete style.css and style.min.css before we minify and optimize
+ */
+gulp.task( 'clean:styles', () =>
+	del( [ 'style.css', 'style.min.css' ] )
+);
+
+/**
+ * Compile Sass and run stylesheet through PostCSS.
+ *
+ * https://www.npmjs.com/package/gulp-sass
+ * https://www.npmjs.com/package/gulp-postcss
+ * https://www.npmjs.com/package/gulp-autoprefixer
+ * https://www.npmjs.com/package/css-mqpacker
+ */
+gulp.task( 'postcss', [ 'clean:styles' ], () =>
+	gulp.src( 'assets/sass/*.scss', paths.css )
+
+	// Deal with errors.
+		.pipe( plumber( {'errorHandler': handleErrors} ) )
+
+		// Wrap tasks in a sourcemap.
+		.pipe( sourcemaps.init() )
+
+		// Compile Sass using LibSass.
+		.pipe( sass( {
+			'includePaths': [].concat( bourbon, neat ),
+			'errLogToConsole': true,
+			'outputStyle': 'expanded' // Options: nested, expanded, compact, compressed
+		} ) )
+
+		// Parse with PostCSS plugins.
+		.pipe( postcss( [
+			autoprefixer( {
+				'browsers': [ 'last 2 version' ]
+			} ),
+			mqpacker( {
+				'sort': true
+			} )
+		] ) )
+
+		// Create sourcemap.
+		.pipe( sourcemaps.write() )
+
+		// Create style.css.
+		.pipe( gulp.dest( './' ) )
+		.pipe( browserSync.stream() )
+);
+
+/**
+ * Minify and optimize style.css.
+ *
+ * https://www.npmjs.com/package/gulp-cssnano
+ */
+gulp.task( 'cssnano', [ 'postcss' ], () =>
+	gulp.src( 'style.css' )
+		.pipe( plumber( {'errorHandler': handleErrors} ) )
+		.pipe( cssnano( {
+			'safe': true // Use safe optimizations.
+		} ) )
+		.pipe( rename( 'style.min.css' ) )
+		.pipe( gulp.dest( './' ) )
+		.pipe( browserSync.stream() )
+);
+
+/**
+ * Sass linting.
+ *
+ * https://www.npmjs.com/package/sass-lint
+ */
+gulp.task( 'sass:lint', () =>
+	gulp.src( [
+		'assets/sass/**/*.scss',
+		'!assets/sass/base/_normalize.scss',
+		'!assets/sass/base/_sprites.scss',
+		'!node_modules/**'
+	] )
+		.pipe( sassLint() )
+		.pipe( sassLint.format() )
+		.pipe( sassLint.failOnError() )
+);
+
+/**
+ * Sass docs.
+ *
+ * http://sassdoc.com/getting-started/
+ */
+gulp.task( 'sassdoc', function() {
+	let options = {
+		dest: 'docs',
+		verbose: true
+	};
+
+	return gulp.src( 'assets/sass/**/*.scss' )
+		.pipe( sassdoc( options ) );
+} );
