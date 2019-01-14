ce00c3335c12d6f85c745bbacc554be0cd178e19
Eric Fuller <info@glassmoonmedia.com>
2019-01-14 12:42:18 -0500

    Refactor the handleErrors function out into its own module.

---
+++ tasks/handle-errors.js
@@ -0,0 +1,21 @@
+const gutil = require( 'gulp-util' );
+const notify = require( 'gulp-notify' );
+/**
+ * Handle errors and alert the user.
+ */
+function handleErrors() {
+	const args = Array.prototype.slice.call( arguments );
+
+	notify.onError( {
+		'title': 'Task Failed [<%= error.message %>',
+		'message': 'See console.',
+		'sound': 'Purr' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+	} ).apply( this, args );
+
+	gutil.beep(); // Beep 'sosumi' again.
+
+	// Prevent the 'watch' task from stopping.
+	this.emit( 'end' );
+}
+
+module.exports = handleErrors;
