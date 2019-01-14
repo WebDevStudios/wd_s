5ea4029f83c74257bb583b5019b299a5707dfb7c
Eric Fuller <info@glassmoonmedia.com>
2019-01-14 15:19:47 -0500

    Move POT file tasks into their own file.

---
+++ tasks/translation.js
@@ -0,0 +1,31 @@
+const del = require( 'del' );
+const gulp = require( 'gulp' );
+const plumber = require( 'gulp-plumber' );
+const wpPot = require( 'gulp-wp-pot' );
+const sort = require( 'gulp-sort' );
+
+const paths = require( './config' ).paths;
+const handleErrors = require( './handle-errors' );
+
+/**
+ * Delete the theme's .pot before we create a new one.
+ */
+gulp.task( 'clean:pot', () =>
+	del( [ 'languages/_s.pot' ] )
+);
+
+/**
+ * Scan the theme and create a POT file.
+ *
+ * https://www.npmjs.com/package/gulp-wp-pot
+ */
+gulp.task( 'wp-pot', [ 'clean:pot' ], () =>
+	gulp.src( paths.php )
+		.pipe( plumber( {'errorHandler': handleErrors} ) )
+		.pipe( sort() )
+		.pipe( wpPot( {
+			'domain': '_s',
+			'package': '_s'
+		} ) )
+		.pipe( gulp.dest( 'languages/_s.pot' ) )
+);
