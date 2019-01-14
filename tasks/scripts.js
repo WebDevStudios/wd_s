44525666b51b0be230e3082555c5542f55ca54b8
Eric Fuller <info@glassmoonmedia.com>
2019-01-14 15:58:20 -0500

    Move scripts and watch into their own files.

---
+++ tasks/scripts.js
@@ -0,0 +1,19 @@
+const gulp = require( 'gulp' );
+const webpack = require( 'webpack' );
+const webpackStream = require( 'webpack-stream' );
+
+const webpackConfig = require( '../webpack.config' );
+
+/**
+ * Let Webpack transpile the JavaScript.
+ */
+gulp.task( 'scripts', function() {
+	gulp.src('./assets/scripts/src/index.js')
+		.pipe(webpackStream(webpackConfig), webpack)
+		.pipe(gulp.dest('./assets/scripts'));
+} );
+
+/**
+ * Individual tasks.
+ */
+gulp.task( 'js', [ 'scripts' ] );
---
+++ tasks/watch.js
@@ -0,0 +1,37 @@
+const browserSync = require( 'browser-sync' );
+const gulp = require( 'gulp' );
+const webpack = require( 'webpack' );
+const webpackDevMiddleware = require( 'webpack-dev-middleware' );
+const webpackHotMiddleware = require( 'webpack-hot-middleware' );
+
+const webpackConfig = require( '../webpack.config' );
+const bundle = webpack( webpackConfig );
+const paths = require( './config' ).paths;
+
+/**
+ * Process tasks and reload browsers on file changes.
+ *
+ * https://www.npmjs.com/package/browser-sync
+ */
+gulp.task( 'watch', function() {
+
+	// Kick off BrowserSync.
+	browserSync( {
+		'open': false, // Open project in a new tab?
+		'injectChanges': true,  // Auto inject changes instead of full reload.
+		'proxy': 'http://hacker.local', // Use https://_s.test:3000 to use BrowserSync.
+		middleware: [
+			webpackDevMiddleware( bundle, {
+				publicPath: 'http://localhost:3000/wp-content/themes/wd_s/assets/scripts/'
+			} ),
+			webpackHotMiddleware( bundle, {} )
+		]
+	} );
+
+	// Run tasks when files change.
+	gulp.watch( paths.icons, [ 'icons' ] );
+	gulp.watch( paths.sass, [ 'styles' ] );
+	gulp.watch( paths.sprites, [ 'sprites' ] );
+	gulp.watch( paths.php, [ 'markup' ] );
+	gulp.watch( paths.js, [ 'scripts' ] );
+} );
