253ad7d89fb13d7689fd007d1d28df4375cc23eb
Eric Fuller <info@glassmoonmedia.com>
2019-01-14 12:41:56 -0500

    Refactor out the config object into its own file.

---
+++ tasks/config.js
@@ -0,0 +1,9 @@
+exports.paths = {
+		'css': [ './*.css', '!*.min.css' ],
+		'icons': 'assets/images/svg-icons/*.svg',
+		'images': [ 'assets/images/*', '!assets/images/*.svg' ],
+		'php': [ './*.php', './**/*.php' ],
+		'js': 'assets/scripts/src/**/*.js',
+		'sass': 'assets/sass/**/*.scss',
+		'sprites': 'assets/images/sprites/*.png'
+};
