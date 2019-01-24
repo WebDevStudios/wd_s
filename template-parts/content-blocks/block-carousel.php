196b2be2a6a9e705d52ef12abcb99ab0eb080575
Corey Collins <coreymcollins@gmail.com>
2019-01-24 11:39:45 -0600

    Adjusts carousel markup and class names to pull in color and animation

--- template-parts/content-blocks/block-carousel.php
+++ template-parts/content-blocks/block-carousel.php
@@ -5,12 +5,14 @@
  * @package _s
  */
 
-$classnames  = ' content-block carousel-block';
-$classnames .= ' ' . get_sub_field( 'display_options' )['block_width'];
+$classnames[] = 'content-block carousel-block';
+$classnames[] = _s_get_animation_class();
+$classnames[] = get_sub_field( 'display_options' )['block_width'];
+$classnames[] = get_sub_field( 'display_options' )['font_color']['color_picker'] ? 'has-font-color color-' . get_sub_field( 'display_options' )['font_color']['color_picker'] : '';
 
 // Start repeater markup...
 if ( have_rows( 'carousel_slides' ) ) :
-	echo '<div class="content-block carousel-block' . esc_attr( $classnames ) . '">';
+	echo '<div class="' . esc_attr( implode( ' ', array_filter( $classnames ) ) ) . '">';
 
 	// Enqueue Slick carousel.
 	_s_enqueue_slick_scripts();
@@ -24,7 +26,6 @@ if ( have_rows( 'carousel_slides' ) ) :
 		$text            = get_sub_field( 'text' );
 		$button_text     = get_sub_field( 'button_text' );
 		$button_url      = get_sub_field( 'button_url' );
-		$animation_class = _s_get_animation_class();
 		$other_options   = get_sub_field( 'other_options' ) ? get_sub_field( 'other_options' ) : get_field( 'other_options' )['other_options'];
 
 		// If the block has expired, then bail!
@@ -42,7 +43,7 @@ if ( have_rows( 'carousel_slides' ) ) :
 			'id'        => esc_attr( 'carousel-' . get_row_index() ),
 		) );
 		?>
-			<div class="slide-content container" data-animation="<?php echo esc_attr( $animation_class ); ?>">
+			<div class="slide-content container">
 
 				<?php if ( $title ) : ?>
 					<h2 class="slide-title"><?php echo esc_html( $title ); ?></h2>
