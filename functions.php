2abb31a5c9f48a9d2567e68f972c9ec3eebcdf62
Corey Collins <coreymcollins@gmail.com>
2019-01-25 14:02:31 -0600

    Adds responsive embed support (and it’s pretty neat)

--- functions.php
+++ functions.php
@@ -114,6 +114,9 @@ if ( ! function_exists( '_s_setup' ) ) :
 		// Gutenberg editor styles support.
 		add_theme_support( 'editor-styles' );
 		add_editor_style( 'style-editor.css' );
+
+		// Gutenberg responsive embed support.
+		add_theme_support( 'responsive-embeds' );
 	}
 endif; // _s_setup
 add_action( 'after_setup_theme', '_s_setup' );
