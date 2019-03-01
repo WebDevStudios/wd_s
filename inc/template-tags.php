198c12cb68973bfd49f8864fb9359af09ca3265f
jomurgel <dev@jomurgel.com>
2019-03-01 08:13:25 -0700

    adds function to test for array key and validates value

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -555,3 +555,22 @@ function _s_display_mobile_menu() {
 	</nav>
 	<?php
 }
+
+/**
+ * Return bool for button text.
+ *
+ * @param [string] $key link array key.
+ * @param [array]  $array link array.
+ * @author jomurgel <jo@webdevstudios.com>
+ * @since NEXT
+ *
+ * @return bool
+ */
+function _s_has_array_key( $key, $array = [] ) {
+
+	if ( ! is_array( $array ) || ( ! $array || ! $key ) ) {
+		return false;
+	}
+
+	return is_array( $array ) && array_key_exists( $key, $array ) && ! empty( $array[ $key ] ); // @codingStandardsIgnoreLine.
+}
