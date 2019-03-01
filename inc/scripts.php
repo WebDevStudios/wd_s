e793c09a061b2c857b0acb2abf9ed16c69c586c2
Jeffrey de Wit <jeffrey@webdevstudios.com>
2019-03-01 12:18:18 -0500

    Add wp-a11y as a requirement for the scripts

--- inc/scripts.php
+++ inc/scripts.php
@@ -82,7 +82,7 @@ function _s_scripts() {
 		wp_enqueue_script( '_s-babel-polyfill', get_template_directory_uri() . '/assets/scripts/babel-polyfill.min.js', array(), $version, true );
 	}
 
-	wp_enqueue_script( '_s-scripts', get_template_directory_uri() . '/assets/scripts/project' . $suffix . '.js', array( 'jquery' ), $version, true );
+	wp_enqueue_script( '_s-scripts', get_template_directory_uri() . '/assets/scripts/project' . $suffix . '.js', array( 'jquery', 'wp-a11y' ), $version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
