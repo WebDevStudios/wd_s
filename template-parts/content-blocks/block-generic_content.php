dd4aa0b373feac086bfba1ad9c065d1f393501fd
jomurgel <dev@jomurgel.com>
2019-03-01 09:28:15 -0700

    removes title from generic content (does not exist)

--- template-parts/content-blocks/block-generic_content.php
+++ template-parts/content-blocks/block-generic_content.php
@@ -6,7 +6,6 @@
  */
 
 // Set up fields.
-$title   = get_sub_field( 'title' );
 $content = get_sub_field( 'content' );
 
 // Start a <container> with possible block options.
@@ -19,13 +18,7 @@ _s_display_block_options(
 ?>
 	<div class="container<?php echo esc_attr( _s_get_animation_class() ); ?>">
 
-		<?php if ( $title ) : ?>
-			<h2 class="generic-content-title"><?php echo esc_html( $title ); ?></h2>
-		<?php endif; ?>
-
-		<?php
-			echo _s_get_the_content( $content ); // WP XSS OK.
-		?>
+		<?php echo _s_get_the_content( $content ); // WP XSS OK. ?>
 
 	</div><!-- .grid-x -->
 </section><!-- .generic-content -->
