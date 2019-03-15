0afd964d49cf040199f704798e8dffe19f22582f
Eric Fuller <info@glassmoonmedia.com>
2019-03-15 15:01:41 -0400

    No need to have code for hot module replacement.

--- assets/scripts/src/index.js
+++ assets/scripts/src/index.js
@@ -8,7 +8,3 @@ import './components/navigation-primary';
 import './components/off-canvas';
 import './components/skip-link-focus-fix';
 import './components/window-ready';
-
-if ( module.hot ) {
-	module.hot.accept();
-}
