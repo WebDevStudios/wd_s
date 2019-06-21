1b9457eb1f4506b238046262f0722f1d36850995
Corey Collins <coreymcollins@gmail.com>
2019-06-21 13:04:42 -0500

    Updates our JS for Tiny Slider

--- assets/scripts/concat/carousel.js
+++ assets/scripts/concat/carousel.js
@@ -26,64 +26,11 @@ window.wdsCarousel = {};
 	// Combine all events.
 	app.bindEvents = function() {
 		app.$c.window.on( 'load', app.doSlick );
-		app.$c.window.on( 'load', app.doFirstAnimation );
 	};
 
 	// Do we meet the requirements?
 	app.meetsRequirements = function() {
 		return app.$c.theCarousel.length;
-	};
-
-	// Animate the first slide on window load.
-	app.doFirstAnimation = function() {
-
-		// Get the first slide content area and animation attribute.
-		let firstSlide = app.$c.theCarousel.find( '[data-slick-index=0]' ),
-			firstSlideContent = firstSlide.find( '.slide-content' ),
-			firstAnimation = firstSlideContent.attr( 'data-animation' );
-
-		// Add the animation class to the first slide.
-		firstSlideContent.addClass( firstAnimation );
-	};
-
-	// Animate the slide content.
-	app.doAnimation = function() {
-		let slides = $( this ).find( '.slide' ),
-			activeSlide = $( this ).find( '.slick-current' ),
-			activeContent = activeSlide.find( '.slide-content' ),
-
-			// This is a string like so: 'animated someCssClass'.
-			animationClass = activeContent.attr( 'data-animation' );
-
-			if ( undefined === animationClass ) {
-				return;
-			}
-
-			let splitAnimation = animationClass.split( ' ' ),
-
-			// This is the 'animated' class.
-			animationTrigger = splitAnimation[0];
-
-		// Go through each slide to see if we've already set animation classes.
-		slides.each( function() {
-			let slideContent = $( this ).find( '.slide-content' );
-
-			// If we've set animation classes on a slide, remove them.
-			if ( slideContent.hasClass( 'animated' ) ) {
-
-				// Get the last class, which is the animate.css class.
-				let lastClass = slideContent
-					.attr( 'class' )
-					.split( ' ' )
-					.pop();
-
-				// Remove both animation classes.
-				slideContent.removeClass( lastClass ).removeClass( animationTrigger );
-			}
-		} );
-
-		// Add animation classes after slide is in view.
-		activeContent.addClass( animationClass );
 	};
 
 	// Allow background videos to autoplay.
@@ -101,16 +48,15 @@ window.wdsCarousel = {};
 	app.doSlick = function() {
 		app.$c.theCarousel.on( 'init', app.playBackgroundVideos );
 
-		app.$c.theCarousel.slick( {
+		var slider = tns( {
+			container: '.carousel-block',
+			items: 1,
+			slideBy: 'page',
 			autoplay: true,
-			autoplaySpeed: 5000,
-			arrows: true,
-			dots: true,
-			focusOnSelect: true,
-			waitForAnimate: true
+			navPosition: 'bottom',
+			autoplayPosition: 'bottom',
+			autoplayTimeout: "2000",
 		} );
-
-		app.$c.theCarousel.on( 'afterChange', app.doAnimation );
 	};
 
 	// Engage!
