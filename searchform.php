5d10c8ef9f17f2955a2f6491fabcb5610345aec5
Greg Rickaby <gregrickaby@gmail.com>
2019-08-27 11:50:46 -0500

    wrap input in label

--- searchform.php
+++ searchform.php
@@ -8,7 +8,9 @@
 ?>
 
 <form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="search-field"><span class="screen-reader-text"><?php esc_html_e( 'To search this site, enter a search term', '_s' ); ?></span></label>
-	<input class="search-field" id="search-field" type="text" name="s" value="<?php echo get_search_query(); ?>" aria-required="false" autocomplete="off" placeholder="<?php echo esc_attr_e( 'Search', '_s' ); ?>" />
-	<input type="submit" id="search-submit" class="button button-search" value="<?php esc_attr_e( 'Submit', '_s' ); ?>" />
+	<label for="search-field">
+		<span class="screen-reader-text"><?php esc_html_e( 'To search this site, enter a search term', '_s' ); ?></span>
+		<input class="search-field" id="search-field" type="text" name="s" value="<?php echo get_search_query(); ?>" aria-required="false" autocomplete="off" placeholder="<?php echo esc_attr_e( 'Search', '_s' ); ?>" />
+	</label>
+	<input type="submit" id="search-submit" class="button button-search" tabindex="-1" value="<?php esc_attr_e( 'Submit', '_s' ); ?>" />
 </form>
