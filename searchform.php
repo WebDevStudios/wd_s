fe4bbd52d57081d3208ad905a32e15238654e441
Corey Collins <coreymcollins@gmail.com>
2019-09-18 14:49:12 -0500

    Adds unique identifiers to form labels and inputs

--- searchform.php
+++ searchform.php
@@ -5,12 +5,14 @@
  * @package _s
  */
 
+// Make sure our search forms have unique IDs in the event more than 1 is on a page.
+$random_identifier = wp_rand();
 ?>
 
 <form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="search-field">
+	<label for="search-field-<?php echo esc_attr( $random_identifier ); ?>">
 		<span class="screen-reader-text"><?php esc_html_e( 'To search this site, enter a search term', '_s' ); ?></span>
-		<input class="search-field" id="search-field" type="text" name="s" value="<?php echo get_search_query(); ?>" aria-required="false" autocomplete="off" placeholder="<?php echo esc_attr_e( 'Search', '_s' ); ?>" />
+		<input class="search-field" id="search-field-<?php echo esc_attr( $random_identifier ); ?>" type="text" name="s" value="<?php echo get_search_query(); ?>" aria-required="false" autocomplete="off" placeholder="<?php echo esc_attr_e( 'Search', '_s' ); ?>" />
 	</label>
 	<input type="submit" id="search-submit" class="button button-search" tabindex="-1" value="<?php esc_attr_e( 'Submit', '_s' ); ?>" />
 </form>
