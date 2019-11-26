9521a05b5e50ea6a7b0f20312b5fa50982b2023c
Corey Collins <coreymcollins@gmail.com>
2019-11-26 15:59:43 -0600

    Updates script paths

--- inc/scripts.php
+++ inc/scripts.php
@@ -74,8 +74,8 @@ function _s_scripts() {
 
 	// Register styles & scripts.
 	wp_register_style( '_s-google-font', _s_font_url(), array(), null ); // @codingStandardsIgnoreLine - required to avoid Google caching issues.
-	wp_register_style( 'slick-carousel', get_template_directory_uri() . '/assets/bower_components/slick-carousel/slick/slick.css', null, '1.8.1' );
-	wp_register_script( 'slick-carousel', get_template_directory_uri() . '/assets/bower_components/slick-carousel/slick/slick' . $suffix . '.js', array( 'jquery' ), '1.8.1', true );
+	wp_register_style( 'slick-carousel', get_template_directory_uri() . '/node_modules/slick-carousel/slick/slick.css', null, '1.8.1' );
+	wp_register_script( 'slick-carousel', get_template_directory_uri() . '/node_modules/slick-carousel/slick/slick' . $suffix . '.js', array( 'jquery' ), '1.8.1', true );
 
 	// Enqueue styles.
 	wp_enqueue_style( '_s-google-font' );
@@ -86,7 +86,7 @@ function _s_scripts() {
 		wp_enqueue_script( '_s-babel-polyfill', get_template_directory_uri() . '/assets/scripts/babel-polyfill.min.js', array(), $version, true );
 	}
 
-	wp_enqueue_script( '_s-scripts', get_template_directory_uri() . '/assets/scripts/project' . $suffix . '.js', array( 'jquery' ), $version, true );
+	wp_enqueue_script( '_s-scripts', get_template_directory_uri() . '/dist/js/app' . $suffix . '.js', array( 'jquery' ), $version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
