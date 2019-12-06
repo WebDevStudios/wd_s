f75db60daa3920a3ad058f00294a402d3cdd9743
Corey Collins <coreymcollins@gmail.com>
2019-12-06 14:07:20 -0600

    Updates window ready/body class from jQuery to vanilla JS

--- src/js/concat/window-ready.js
+++ src/js/concat/window-ready.js
@@ -3,32 +3,12 @@
  *
  * Add a "ready" class to <body> when window is ready.
  */
-window.wdsWindowReady = {};
-( function( window, $, app ) {
-	// Constructor.
-	app.init = function() {
-		app.cache();
-		app.bindEvents();
-	};
+function wdsWindowReady() {
+	document.body.classList.add( 'ready' );
+}
 
-	// Cache document elements.
-	app.cache = function() {
-		app.$c = {
-			window: $( window ),
-			body: $( document.body ),
-		};
-	};
-
-	// Combine all events.
-	app.bindEvents = function() {
-		app.$c.window.load( app.addBodyClass );
-	};
-
-	// Add a class to <body>.
-	app.addBodyClass = function() {
-		app.$c.body.addClass( 'ready' );
-	};
-
-	// Engage!
-	$( app.init );
-}( window, jQuery, window.wdsWindowReady ) );
+if ( 'complete' === document.readyState || 'loading' !== document.readyState && ! document.documentElement.doScroll ) {
+	wdsWindowReady();
+} else {
+	document.addEventListener( 'DOMContentLoaded', wdsWindowReady );
+}
