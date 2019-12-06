2e549cb969abdb7368187ca8dfff65e2b28e5844
Corey Collins <coreymcollins@gmail.com>
2019-12-06 11:01:13 -0600

    Converts header button jQuery to vanilla JS

--- src/js/concat/header-button.js
+++ src/js/concat/header-button.js
@@ -3,76 +3,50 @@
  *
  * @author Corey Collins
  */
-window.ShowHideSearchForm = {};
-( function( window, $, app ) {
-	// Constructor
-	app.init = function() {
-		app.cache();
 
-		if ( app.meetsRequirements() ) {
-			app.bindEvents();
-		}
-	};
+( function() {
+	const headerSearch = document.querySelector( '.site-header-action' ),
+		headerSearchToggle = document.querySelector( '.site-header-action .cta-button' ),
+		headerSearchForm = document.querySelector( '.site-header-action .form-container' );
 
-	// Cache all the things
-	app.cache = function() {
-		app.$c = {
-			window: $( window ),
-			body: $( 'body' ),
-			headerSearchToggle: $( '.site-header-action .cta-button' ),
-			headerSearchForm: $( '.site-header-action .form-container' ),
-		};
-	};
+	headerSearchToggle.addEventListener( 'click', toggleSearchForm );
+	document.body.addEventListener( 'keyup', hideSearchForm );
+	document.body.addEventListener( 'touchstart', hideSearchForm );
+	document.body.addEventListener( 'click', hideSearchForm );
 
-	// Combine all events
-	app.bindEvents = function() {
-		app.$c.headerSearchToggle.on( 'keyup click', app.showHideSearchForm );
-		app.$c.body.on( 'keyup touchstart click', app.hideSearchForm );
-	};
-
-	// Do we meet the requirements?
-	app.meetsRequirements = function() {
-		return app.$c.headerSearchToggle.length;
-	};
-
-	// Checks to see if the menu has been opened.
-	app.searchIsOpen = function() {
-		if ( app.$c.body.hasClass( 'search-form-visible' ) ) {
+	// Check to see if the search is open or not.
+	function searchIsOpen() {
+		if ( document.body.classList.contains( 'search-form-visible' ) ) {
 			return true;
 		}
 
 		return false;
-	};
+	}
 
-	// Adds the toggle class for the search form.
-	app.showHideSearchForm = function() {
-		app.$c.body.toggleClass( 'search-form-visible' );
+	// Toggle our form display by changing body classes and aria labels.
+	function toggleSearchForm() {
+		document.body.classList.toggle( 'search-form-visible' );
+		toggleAriaLabels();
+	}
 
-		app.toggleSearchFormAriaLabel();
-		app.toggleSearchToggleAriaLabel();
-
-		return false;
-	};
-
-	// Hides the search form if we click outside of its container.
-	app.hideSearchForm = function( event ) {
-		if ( ! $( event.target ).parents( 'div' ).hasClass( 'site-header-action' ) ) {
-			app.$c.body.removeClass( 'search-form-visible' );
-			app.toggleSearchFormAriaLabel();
-			app.toggleSearchToggleAriaLabel();
+	// Toggle the aria label values.
+	function toggleAriaLabels() {
+		if ( searchIsOpen() ) {
+			headerSearchForm.setAttribute( 'aria-hidden', 'false' );
+			headerSearchToggle.setAttribute( 'aria-expanded', 'true' );
+		} else {
+			headerSearchForm.setAttribute( 'aria-hidden', 'true' );
+			headerSearchToggle.setAttribute( 'aria-expanded', 'false' );
 		}
-	};
+	}
 
-	// Toggles the aria-hidden label on the form container.
-	app.toggleSearchFormAriaLabel = function() {
-		app.$c.headerSearchForm.attr( 'aria-hidden', app.searchIsOpen() ? 'false' : 'true' );
-	};
+	// Hide the form if we click or tab outside of its container.
+	function hideSearchForm( event ) {
+		const isTargetInside = headerSearch.contains( event.target );
 
-	// Toggles the aria-hidden label on the toggle button.
-	app.toggleSearchToggleAriaLabel = function() {
-		app.$c.headerSearchToggle.attr( 'aria-expanded', app.searchIsOpen() ? 'true' : 'false' );
-	};
-
-	// Engage
-	$( app.init );
-}( window, jQuery, window.ShowHideSearchForm ) );
+		if ( ! isTargetInside ) {
+			document.body.classList.remove( 'search-form-visible' );
+			toggleAriaLabels();
+		}
+	}
+}() );
