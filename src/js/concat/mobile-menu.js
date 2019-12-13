0845abc25594e7ecd69479b8ad536cbd100fa4e6
Corey Collins <coreymcollins@gmail.com>
2019-12-13 15:00:26 -0600

    Converts the mobile menu jQuery to vanilla JS

--- src/js/concat/mobile-menu.js
+++ src/js/concat/mobile-menu.js
@@ -3,135 +3,98 @@
  *
  * Create an accordion style dropdown.
  */
-window.wdsMobileMenu = {};
-( function( window, $, app ) {
-	// Constructor.
-	app.init = function() {
-		app.cache();
 
-		if ( app.meetsRequirements() ) {
-			app.bindEvents();
-		}
-	};
+// Make sure everything is loaded first.
+if ( 'complete' === document.readyState || 'loading' !== document.readyState && ! document.documentElement.doScroll ) {
+	wdsMobileMenu();
+} else {
+	document.addEventListener( 'DOMContentLoaded', wdsMobileMenu );
+}
 
-	// Cache all the things.
-	app.cache = function() {
-		app.$c = {
-			body: $( 'body' ),
-			window: $( window ),
-			subMenuContainer: $( '.mobile-menu .sub-menu, .utility-navigation .sub-menu' ),
-			subSubMenuContainer: $( '.mobile-menu .sub-menu .sub-menu' ),
-			subMenuParentItem: $( '.mobile-menu li.menu-item-has-children, .utility-navigation li.menu-item-has-children' ),
-			offCanvasContainer: $( '.off-canvas-container' ),
-		};
-	};
+// Handle our mobile menus.
+function wdsMobileMenu() {
+	const subMenuParentItem = document.querySelectorAll( '.mobile-menu li.menu-item-has-children, .utility-navigation li.menu-item-has-children' );
 
-	// Combine all events.
-	app.bindEvents = function() {
-		app.$c.window.on( 'load', app.addDownArrow );
-		app.$c.subMenuParentItem.on( 'click', app.toggleSubmenu );
-		app.$c.subMenuParentItem.on( 'transitionend', app.resetSubMenu );
-		app.$c.offCanvasContainer.on( 'transitionend', app.forceCloseSubmenus );
-	};
+	subMenuParentItem.forEach( ( subMenuParent ) => {
+		const menuItem = subMenuParent.querySelector( 'a' );
 
-	// Do we meet the requirements?
-	app.meetsRequirements = function() {
-		return app.$c.subMenuContainer.length;
-	};
+		menuItem.innerHTML += '<button type="button" aria-expanded="false" class="parent-indicator" aria-label="Open submenu"><span class="down-arrow"></span></button>';
 
-	// Reset the submenus after it's done closing.
-	app.resetSubMenu = function() {
-		// When the list item is done transitioning in height,
-		// remove the classes from the submenu so it is ready to toggle again.
-		if ( $( this ).is( 'li.menu-item-has-children' ) && ! $( this ).hasClass( 'is-visible' ) ) {
-			$( this ).find( 'ul.sub-menu' ).removeClass( 'slideOutLeft is-visible' );
-		}
-	};
+		const subMenuTrigger = document.querySelectorAll( '.parent-indicator' );
 
-	// Slide out the submenu items.
-	app.slideOutSubMenus = function( el ) {
-		// If this item's parent is visible and this is not, bail.
-		if ( el.parent().hasClass( 'is-visible' ) && ! el.hasClass( 'is-visible' ) ) {
-			return;
-		}
-
-		// If this item's parent is visible and this item is visible, hide its submenu then bail.
-		if ( el.parent().hasClass( 'is-visible' ) && el.hasClass( 'is-visible' ) ) {
-			el.removeClass( 'is-visible' ).find( '.sub-menu' ).removeClass( 'slideInLeft' ).addClass( 'slideOutLeft' );
-			return;
-		}
-
-		app.$c.subMenuContainer.each( function() {
-			// Only try to close submenus that are actually open.
-			if ( $( this ).hasClass( 'slideInLeft' ) ) {
-				// Close the parent list item, and set the corresponding button aria to false.
-				$( this ).parent().removeClass( 'is-visible' ).find( '.parent-indicator' ).attr( 'aria-expanded', false );
-
-				// Slide out the submenu.
-				$( this ).removeClass( 'slideInLeft' ).addClass( 'slideOutLeft' );
-			}
+		subMenuTrigger.forEach( ( trigger ) => {
+			trigger.addEventListener( 'click', toggleSubmenu );
 		} );
-	};
+	} );
 
-	// Add the down arrow to submenu parents.
-	app.addDownArrow = function() {
-		app.$c.subMenuParentItem.find( 'a:first' ).after( '<button type="button" aria-expanded="false" class="parent-indicator" aria-label="Open submenu"><span class="down-arrow"></span></button>' );
-	};
+	// Open/Close a submenu.
+	function toggleSubmenu( event ) {
+		event.preventDefault();
 
-	// Deal with the submenu.
-	app.toggleSubmenu = function( e ) {
-		const el = $( this ), // The menu element which was clicked on.
-			subMenu = el.children( 'ul.sub-menu' ), // The nearest submenu.
-			$target = $( e.target ); // the element that's actually being clicked (child of the li that triggered the click event).
+		const targetElement = event.target,
+			targetParent = targetElement.parentNode.closest( '.menu-item-has-children' ),
+			subMenu = targetParent.querySelector( 'ul.sub-menu' );
 
-		// Figure out if we're clicking the button or its arrow child,
-		// if so, we can just open or close the menu and bail.
-		if ( $target.hasClass( 'down-arrow' ) || $target.hasClass( 'parent-indicator' ) ) {
-			// First, collapse any already opened submenus.
-			app.slideOutSubMenus( el );
-
-			if ( ! subMenu.hasClass( 'is-visible' ) ) {
-				// Open the submenu.
-				app.openSubmenu( el, subMenu );
-			}
-			return false;
-		}
-	};
+		closeAllSubmenus( targetParent );
+		maybeOpenSubmenu( targetParent, subMenu );
+	}
 
 	// Open a submenu.
-	app.openSubmenu = function( parent, subMenu ) {
+	function maybeOpenSubmenu( parent, subMenu ) {
+
+		if ( parent.classList.contains( 'is-visible' ) ) {
+			closeSubmenu( parent, subMenu );
+			return;
+		}
+
 		// Expand the list menu item, and set the corresponding button aria to true.
-		parent.addClass( 'is-visible' ).find( '.parent-indicator' ).attr( 'aria-expanded', true );
+		parent.classList.add( 'is-visible' );
+		parent.querySelector( '.parent-indicator' ).setAttribute( 'aria-expanded', true );
 
 		// Slide the menu in.
-		subMenu.addClass( 'is-visible animated slideInLeft' );
+		subMenu.classList.add( 'is-visible', 'animated', 'slideInLeft' );
 	};
 
-	// Force close all the submenus when the main menu container is closed.
-	app.forceCloseSubmenus = function( event ) {
-		if ( $( event.target ).hasClass( 'off-canvas-container' ) ) {
-			// Focus offcanvas menu for a11y.
-			app.$c.offCanvasContainer.focus();
+	// Close a submenu.
+	function closeSubmenu( parent, subMenu ) {
+		parent.classList.remove( 'is-visible' );
+		parent.querySelector( '.parent-indicator' ).setAttribute( 'aria-expanded', false );
+		subMenu.classList.remove( 'is-visible', 'animated', 'slideInLeft' );
+	};
 
-			// The transitionend event triggers on open and on close, need to make sure we only do this on close.
-			if ( ! $( this ).hasClass( 'is-visible' ) ) {
-				app.$c.subMenuParentItem.removeClass( 'is-visible' ).find( '.parent-indicator' ).attr( 'aria-expanded', false );
-				app.$c.subMenuContainer.removeClass( 'is-visible slideInLeft' );
-				app.$c.body.css( 'overflow', 'visible' );
-				app.$c.body.unbind( 'touchstart' );
+	// Close all open submenus on the same level/hierarchy as the menu we're trying to open.
+	function closeAllSubmenus( targetParent ) {
+
+		const submenuSiblings = getSiblings( targetParent );
+
+		submenuSiblings.forEach( ( sibling ) => {
+			sibling.classList.remove( 'is-visible' );
+
+			if ( sibling.querySelector( '.parent-indicator' ) ) {
+				sibling.querySelector( '.parent-indicator' ).setAttribute( 'aria-expanded', false );
 			}
 
-			if ( $( this ).hasClass( 'is-visible' ) ) {
-				app.$c.body.css( 'overflow', 'hidden' );
-				app.$c.body.bind( 'touchstart', function( e ) {
-					if ( ! $( e.target ).parents( '.contact-modal' )[ 0 ] ) {
-						e.preventDefault();
-					}
-				} );
+			if ( sibling.querySelector( '.sub-menu' ) ) {
+				sibling.querySelector( '.sub-menu' ).classList.remove( 'is-visible', 'animated', 'slideInLeft' );
 			}
+		} );
+	}
+
+	// Find siblings of an item.
+	let getSiblings = function( element ) {
+
+		let siblings = [];
+		let sibling = element.parentNode.firstChild;
+
+		while ( sibling ) {
+
+			if ( sibling.nodeType === 1 && sibling !== element ) {
+				siblings.push( sibling );
+			}
+
+			sibling = sibling.nextSibling;
 		}
-	};
 
-	// Engage!
-	$( app.init );
-}( window, jQuery, window.wdsMobileMenu ) );
+		return siblings;
+	};
+}
