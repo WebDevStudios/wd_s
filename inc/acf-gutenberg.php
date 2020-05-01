96cb49b25f1c6c20927851576fb1b03a38805db7
Corey Collins <coreymcollins@gmail.com>
2020-05-01 15:57:04 -0500

    Updates script paths

--- inc/acf-gutenberg.php
+++ inc/acf-gutenberg.php
@@ -289,8 +289,8 @@ function _s_acf_enqueue_carousel_scripts() {
 	}
 
 	_s_acf_enqueue_backend_block_styles();
-	wp_register_style( 'slick-carousel', get_template_directory_uri() . '/assets/slick-carousel/slick/slick.css', null, '1.8.1' );
-	wp_register_script( 'slick-carousel-js', get_template_directory_uri() . '/assets/slick-carousel/slick/slick.min.js', array( 'jquery' ), '1.8.1', true );
+	wp_register_style( 'slick-carousel', get_template_directory_uri() . '/assets/slick-carousel/slick.css', null, '1.8.1' );
+	wp_register_script( 'slick-carousel-js', get_template_directory_uri() . '/assets/slick-carousel/slick.min.js', array( 'jquery' ), '1.8.1', true );
 	wp_enqueue_style( 'slick-carousel' );
 	wp_enqueue_script( 'slick-carousel-js' );
 	wp_enqueue_script( 'wds-block-js', get_template_directory_uri() . '/dist/js/app.min.js', array( 'slick-carousel-js' ), '1.0.0', true );
