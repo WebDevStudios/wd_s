85ec478ce67a6c694e93f777f867771a61dc2fb3
Ashar Irfan <mrasharirfan@gmail.com>
2020-09-23 20:57:24 +0500

    Leverage @wordpress/scripts as primary tool chain (#552)
    * 📦 NEW: webpack config file
    
    * 📦 NEW: imagemin-webpack-plugin to optimize images via wp-scripts
    
    * Remove svgo config file
    
    * 👌 IMPROVE: Config comments
    
    * Squashed commit of the following:
    
    Merges tailwind branch down to fix some conflicts
    
    Co-authored-by: Corey Collins <coreymcollins@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/**
+ * wd_s Webpack config.
+ *
+ * This config includes the `@wordpress/scripts` defaults, along with
+ * imagemin-webpack-plugin for optimizing images present in the theme.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-scripts/#provide-your-own-webpack-config
+ */
+const path = require( 'path' );
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
+const { mode } = defaultConfig;
+
+module.exports = {
+	...defaultConfig,
+	plugins: [
+		...defaultConfig.plugins,
+
+		// Copy the all types of images present in `src/images` directory to build directory.
+		new CopyWebpackPlugin( {
+			patterns: [
+				{
+					from: '**/*.{jpg,jpeg,png,gif,svg}',
+					to: 'images/[path][name].[ext]',
+					context: path.resolve( process.cwd(), 'src/images' ),
+				},
+			],
+		} ),
+
+		// This should run after CopyWebpackPlugin.
+		// This plugin optimizes the images copied in the previous step build directory.
+		new ImageminPlugin( {
+			disable: 'production' !== mode,
+			test: /\.(jpe?g|png|gif|svg)$/i,
+			svgo: {
+				plugins: [
+					{ removeViewBox: false },
+					{ removeDimensions: true },
+					{
+						removeAttrs: {
+							attrs: '*:(stroke|fill):((?!^none$).)*',
+						},
+					},
+				],
+			},
+		} ),
+	],
+};
