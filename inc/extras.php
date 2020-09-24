8257103f1466c0d2e000586e8afd8a2074cd970d
Corey Collins <coreymcollins@gmail.com>
2020-09-24 12:51:51 -0500

    Adds the copyright year shortcode

--- inc/extras.php
+++ inc/extras.php
@@ -32,3 +32,35 @@ function _s_categorized_blog() {
 
 	return $category_count > 1;
 }
+
+/**
+ * Shortcode to display copyright year.
+ *
+ * @author Haris Zulfiqar
+ * @param array $atts {.
+ * @type string $starting_year Optional. Define starting year to show starting year and current year e.g. 2015 - 2018.
+ * @type string $separator Optional. Separator between starting year and current year.
+ * }
+ * @return string
+ */
+function _s_copyright_year( $atts ) {
+
+	// Setup defaults.
+	$args = shortcode_atts(
+		array(
+			'starting_year' => '',
+			'separator'     => ' - ',
+		),
+		$atts
+	);
+
+	$current_year = date( 'Y' );
+
+	// Return current year if starting year is empty.
+	if ( ! $args['starting_year'] ) {
+		return $current_year;
+	}
+
+	return esc_html( $args['starting_year'] . $args['separator'] . $current_year );
+}
+add_shortcode( '_s_copyright_year', '_s_copyright_year', 15 );
