91350211407dfb00573aa219a27dbe3bd31fa72f
Corey Collins <coreymcollins@gmail.com>
2020-10-08 08:21:20 -0500

    Updates box shadow to use REM instead of pixel values

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,20 @@ module.exports = {
 			80: '5rem',
 			192: '12rem',
 		},
+		boxShadow: {
+			xs: '0 0 0 0.0625rem rgba(0, 0, 0, 0.05)',
+			sm: '0 0.0625rem 0.125rem 0 rgba(0, 0, 0, 0.05)',
+			default: '0 0.0625rem 0.1875rem 0 rgba(0, 0, 0, 0.1), 0 0.0625rem 0.125rem 0 rgba(0, 0, 0, 0.06)',
+			md: '0 0.25rem 0.375rem -0.0625rem rgba(0, 0, 0, 0.1), 0 0.125rem 0.25rem -0.0625rem rgba(0, 0, 0, 0.06)',
+			lg: '0 0.625rem 0.9375 -0.1875rem rgba(0, 0, 0, 0.1), 0 0.25rem 0.375rem -0.125rem rgba(0, 0, 0, 0.05)',
+			xl: '0 1.25rem 1.5625rem -0.3125rem rgba(0, 0, 0, 0.1), 0 0.625rem 0.625rem -0.3125rem rgba(0, 0, 0, 0.04)',
+			'2xl': '0 1.5625rem 3.125rem -10.125rem rgba(0, 0, 0, 0.25)',
+			'3xl': '0 2.1875rem 3.75rem -0.9375rem rgba(0, 0, 0, 0.3)',
+			inner: 'inset 0 0.125rem 0.25rem 0 rgba(0, 0, 0, 0.06)',
+			outline: '0 0 0 0.1875rem rgba(66, 153, 225, 0.5)',
+			focus: '0 0 0 0.1875rem rgba(66, 153, 225, 0.5)',
+			none: 'none',
+		},
 		extend: {
 			maxHeight: {
 				'0': '0',
