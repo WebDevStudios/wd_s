1a709a53c44c267acf1c5a8b5cd91c6ef0bb1d9d
Corey Collins <coreymcollins@gmail.com>
2020-10-08 14:50:36 -0500

    Adds a function to extras for grabbing attachment IDs

--- inc/extras.php
+++ inc/extras.php
@@ -34,6 +34,43 @@ function _s_categorized_blog() {
 }
 
 /**
+ * Get an attachment ID from it's URL.
+ *
+ * @author WDS
+ * @param string $attachment_url The URL of the attachment.
+ * @return int The attachment ID.
+ */
+function _s_get_attachment_id_from_url( $attachment_url = '' ) {
+
+	global $wpdb;
+
+	$attachment_id = false;
+
+	// If there is no url, return.
+	if ( '' === $attachment_url ) {
+		return false;
+	}
+
+	// Get the upload directory paths.
+	$upload_dir_paths = wp_upload_dir();
+
+	// Make sure the upload path base directory exists in the attachment URL, to verify that we're working with a media library image.
+	if ( false !== strpos( $attachment_url, $upload_dir_paths['baseurl'] ) ) {
+
+		// If this is the URL of an auto-generated thumbnail, get the URL of the original image.
+		$attachment_url = preg_replace( '/-\d+x\d+(?=\.(jpg|jpeg|png|gif)$)/i', '', $attachment_url );
+
+		// Remove the upload path base directory from the attachment URL.
+		$attachment_url = str_replace( $upload_dir_paths['baseurl'] . '/', '', $attachment_url );
+
+		// Do something with $result.
+		$attachment_id = $wpdb->get_var( $wpdb->prepare( "SELECT wposts.ID FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta WHERE wposts.ID = wpostmeta.post_id AND wpostmeta.meta_key = '_wp_attached_file' AND wpostmeta.meta_value = %s AND wposts.post_type = 'attachment'", $attachment_url ) ); // WPCS db call ok, cache ok, placeholder ok.
+	}
+
+	return $attachment_id;
+}
+
+/**
  * Shortcode to display copyright year.
  *
  * @author Haris Zulfiqar
