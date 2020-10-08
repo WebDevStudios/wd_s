266fa17e0426b0cf00e2c2dbdd619c2edf8ec2ee
Corey Collins <coreymcollins@gmail.com>
2020-10-08 08:59:36 -0500

    Cleans up single post styles

--- single.php
+++ single.php
@@ -9,25 +9,23 @@
 
 get_header(); ?>
 
-	<div class="container mx-auto my-12">
-		<main id="main" class="site-main">
+	<main id="main" class="site-main container">
 
-			<?php
-			while ( have_posts() ) :
-				the_post();
+		<?php
+		while ( have_posts() ) :
+			the_post();
 
-				get_template_part( 'template-parts/content', get_post_format() );
+			get_template_part( 'template-parts/content', get_post_format() );
 
-				the_post_navigation();
+			the_post_navigation();
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
 
-			endwhile; // End of the loop.
-			?>
+		endwhile; // End of the loop.
+		?>
 
-		</main><!-- #main -->
-	</div><!-- .grid-wrapper -->
+	</main><!-- #main -->
 <?php get_footer(); ?>
