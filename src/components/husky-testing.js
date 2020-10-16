3018946b9dab3dcd45afe47b2177dfef0c225076
Aubrey Portwood <code@aubreypwd.com>
2020-10-16 07:18:28 -0600

    But this should make it through.

--- src/components/husky-testing.js
+++ src/components/husky-testing.js
@@ -1,3 +1,14 @@
 const test = 'true';
 
-return test;
+/**
+ * Something
+ *
+ * @author Aubrey Portwood <aubrey@webdevstudios.com>
+ * @since  Friday, 10/16/2020
+ * @return {string} Yup
+ */
+function something() {
+	return test;
+}
+
+something();
