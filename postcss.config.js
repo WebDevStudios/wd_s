49a78942eec2378bcc4b4f0c9a3134b904885f6f
Mike England <mike.england@webdevstudios.com>
2021-03-16 17:29:46 -0400

    Convert css nano to conditional include

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,17 @@
-module.exports = {
-	plugins: [
-		require( 'tailwindcss' ),
-		require( 'autoprefixer' ),
+const plugins = [
+	require( 'tailwindcss' ),
+	require( 'autoprefixer' ),
+];
+
+// Include CSS Nano if production mode.
+if ( 'production' === process.env.NODE_ENV ) {
+	plugins.push(
 		require( 'cssnano' )( {
 			preset: 'default',
 		} ),
-	],
+	);
+}
+
+module.exports = {
+	plugins,
 };
