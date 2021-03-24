0bf495befafc8288606cdba131f69ce860344156
Mike England <mike.england@webdevstudios.com>
2021-03-24 17:15:16 -0400

    Setup tailwind entry

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ const { mode } = defaultConfig;
 
 module.exports = {
 	...defaultConfig,
+	entry: {
+		...defaultConfig.entry,
+		tailwind: path.resolve( process.cwd(), 'src/scss/tailwind.scss' ),
+	},
 	plugins: [
 		...defaultConfig.plugins,
 
