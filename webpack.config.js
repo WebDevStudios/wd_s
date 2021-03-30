b3dda9c44da38ec9f27f5e282acb9a3d240ab9c6
Greg Rickaby <gregrickaby@gmail.com>
2021-03-30 12:09:00 -0500

    refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,36 @@
+const path = require('path');
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const CopyPlugin = require('copy-webpack-plugin');
+
 /**
- * wd_s Webpack config.
- *
- * This config includes the `@wordpress/scripts` defaults, along with
- * imagemin-webpack-plugin for optimizing images present in the theme.
+ * Webpack config.
  *
  * @see https://developer.wordpress.org/block-editor/packages/packages-scripts/#provide-your-own-webpack-config
  */
-const path = require( 'path' );
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
-const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
-const { mode } = defaultConfig;
-
 module.exports = {
 	...defaultConfig,
+	mode: 'development',
 	plugins: [
 		...defaultConfig.plugins,
 
-		// Copy the all types of images present in `src/images` directory to build directory.
-		new CopyWebpackPlugin( {
+		/**
+		 * Copy source files/directories to a build directory.
+		 *
+		 * @see https://www.npmjs.com/package/copy-webpack-plugin
+		 */
+		new CopyPlugin({
 			patterns: [
 				{
 					from: '**/*.{jpg,jpeg,png,gif,svg}',
 					to: 'images/[path][name].[ext]',
-					context: path.resolve( process.cwd(), 'src/images' ),
+					context: path.resolve(process.cwd(), 'src/images'),
 				},
 				{
 					from: '*.svg',
 					to: 'images/icons/[name].[ext]',
-					context: path.resolve( process.cwd(), 'src/images/icons' ),
+					context: path.resolve(process.cwd(), 'src/images/icons'),
 				},
 			],
-		} ),
-
-		// This should run after CopyWebpackPlugin.
-		// This plugin optimizes the images copied in the previous step build directory.
-		new ImageminPlugin( {
-			disable: 'production' !== mode,
-			test: /\.(jpe?g|png|gif|svg)$/i,
-			svgo: {
-				plugins: [
-					{ removeViewBox: false },
-					{ removeDimensions: true },
-					{
-						removeAttrs: {
-							attrs: '*:(stroke|fill):((?!^none$).)*',
-						},
-					},
-				],
-			},
-		} ),
+		}),
 	],
 };
