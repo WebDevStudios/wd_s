836b809e44c3a609549a5dcafbb5a387fc662919
Brad Parbs <brad@bradparbs.com>
2021-06-15 10:30:05 -0500

    Clean up Text_Editor_Custom_Control, mark as final, and change script output count to boolean

--- inc/customizer/class-text-editor-custom-control.php
+++ inc/customizer/class-text-editor-custom-control.php
@@ -5,75 +5,73 @@
  * @package _s
  */
 
-if ( class_exists( 'WP_Customize_Control' ) ) :
+if ( ! class_exists( 'WP_Customize_Control' ) ) {
+	return;
+}
+
+/**
+ * Class to create a custom text editor control
+ */
+final class Text_Editor_Custom_Control extends WP_Customize_Control {
 
 	/**
-	 * Class to create a custom text editor control
+	 * Keep track of if scripts were added.
+	 *
+	 * @var $did_scripts
 	 */
-	class Text_Editor_Custom_Control extends WP_Customize_Control {
+	protected static $did_scripts = false;
 
-		/**
-		 * Keep track of how many editors are added.
-		 *
-		 * @var $count
-		 */
-		protected static $count = 0;
+	/**
+	 * Render the content on the theme customizer page
+	 *
+	 * @author WebDevStudios
+	 */
+	public function render_content() {
+		?>
+		<label>
+			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+			<span class="description customize-control-description"><?php echo esc_html( $this->description ); ?></span>
+		</label>
+		<div class="wds-customize-text-editor">
+			<?php
+			// Setttings for the editor.
+			$settings = [
+				'textarea_name' => $this->id,
+				'textarea_rows' => 4,
+				'media_buttons' => true,
+			];
 
-		/**
-		 * Render the content on the theme customizer page
-		 *
-		 * @author WDS
-		 */
-		public function render_content() {
-			?>
-			<label>
-				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-				<span class="description customize-control-description"><?php echo esc_html( $this->description ); ?></span>
-			</label>
-			<div class="wds-customize-text-editor">
-				<?php
-				// Setttings for the editor.
-				$settings = [
-					'textarea_name' => $this->id,
-					'textarea_rows' => 4,
-					'media_buttons' => true,
-				];
+			// Add the editor.
+			wp_editor( $this->value(), $this->id, $settings );
 
-				// Add the editor.
-				wp_editor( $this->value(), $this->id, $settings );
-
-				// Only enqueue scripts once.
-				if ( 0 === self::$count ) {
-					$this->enqueue_scripts();
-				}
-
-				// add the footer scripts.
+			// Only enqueue scripts once.
+			if ( ! self::$did_scripts ) {
+				$this->enqueue_scripts();
 				$this->add_footer_scripts();
 
-				// Increment count.
-				++self::$count;
-				?>
-			</div>
-			<?php
-		}
-
-		/**
-		 * Enqueue scripts.
-		 *
-		 * @author WDS
-		 */
-		protected function enqueue_scripts() {
-			wp_enqueue_script( 'tiny_mce' );
-			wp_enqueue_script( 'wds-customize-editor-js', get_template_directory_uri() . '/inc/customizer/assets/scripts/tinymce.js', [ 'jquery' ], '1.0.0', true );
-		}
-
-		/**
-		 * Add footer scripts for Tinymce.
-		 *
-		 * @author WDS
-		 */
-		protected function add_footer_scripts() {
-			do_action( 'admin_print_footer_scripts' );
-		}
+				self::$did_scripts = true;
+			}
+			?>
+		</div>
+		<?php
 	}
-endif;
+
+	/**
+	 * Enqueue scripts.
+	 *
+	 * @author WebDevStudios
+	 */
+	protected function enqueue_scripts() {
+		wp_enqueue_script( 'tiny_mce' );
+		wp_enqueue_script( 'wds-customize-editor-js', get_template_directory_uri() . '/inc/customizer/assets/scripts/tinymce.js', [ 'jquery' ], '1.0.0', true );
+	}
+
+	/**
+	 * Add footer scripts for Tinymce.
+	 *
+	 * @author WebDevStudios
+	 */
+	protected function add_footer_scripts() {
+		do_action( 'admin_print_footer_scripts' );
+	}
+}
