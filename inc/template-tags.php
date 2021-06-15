65fd0b566c4e11c28251b1bfaf7fc5405a84fe06
Brad Parbs <brad@bradparbs.com>
2021-06-15 10:26:46 -0500

    Simplify copyright year output

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -290,12 +290,9 @@ function _s_display_copyright_text() {
 	// Grab our customizer settings.
 	$copyright_text = get_theme_mod( '_s_copyright_text' );
 
-	// Stop if there's nothing to display.
-	if ( ! $copyright_text ) {
-		return false;
+	if ( $copyright_text ) {
+		echo _s_get_the_content( do_shortcode( $copyright_text ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- XSS OK.
 	}
-
-	echo _s_get_the_content( do_shortcode( $copyright_text ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- XSS OK.
 }
 
 /**
