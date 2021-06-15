885623e749d378d44f0455a9a7772169ab9fefae
Brad Parbs <brad@bradparbs.com>
2021-06-15 10:22:01 -0500

    Use " instead of ' for strings that have a quote in them

--- 404.php
+++ 404.php
@@ -13,12 +13,12 @@ get_header(); ?>
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Sorry, this page doesn\'t exist.', '_s' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( "Sorry, this page doesn't exist.", '_s' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
 
-				<p><?php esc_html_e( 'It seems we can\'t find what you\'re looking for. Perhaps searching can help.', '_s' ); ?></p>
+				<p><?php esc_html_e( "It seems we can't find what you're looking for. Perhaps searching can help.", '_s' ); ?></p>
 
 				<?php get_search_form(); ?>
 
