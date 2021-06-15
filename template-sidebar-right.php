37f062ca05170558c1886422249eb6b1012ccc82
Brad Parbs <brad@bradparbs.com>
2021-06-15 17:09:14 -0500

    Move check for comments being open & if comments, then calling comments_template() to a template tag

--- template-sidebar-right.php
+++ template-sidebar-right.php
@@ -20,10 +20,7 @@ get_header(); ?>
 
 				get_template_part( 'template-parts/content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+				_s_display_comments();
 
 			endwhile; // End of the loop.
 			?>
