f25441b09919988022126376a564881236b8c292
Brad Parbs <brad@bradparbs.com>
2021-06-15 10:22:53 -0500

    Fix indentation of PHP code

--- archive.php
+++ archive.php
@@ -15,8 +15,8 @@ get_header(); ?>
 
 			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
