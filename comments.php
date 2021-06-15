aadf18b3e32d4033e2728e71ae7dd6bcf6db475c
Brad Parbs <brad@bradparbs.com>
2021-06-15 10:21:06 -0500

    Update block comments to be standardized

--- comments.php
+++ comments.php
@@ -10,7 +10,7 @@
  * @package _s
  */
 
-/*
+/**
  * If the current post is protected by a password and
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
