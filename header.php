aec4ccf01a384167b008ee670b26fac63b49b363
Brad Parbs <brad@bradparbs.com>
2021-06-15 19:11:36 -0500

    Update url in header profile rel link to be https

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
