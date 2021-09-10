a81246243641bbf4cbe9a77b14903c4b499b992d
Corey Collins <coreymcollins@gmail.com>
2021-09-10 16:08:22 -0400

    Simplifies webpack config around loaders

--- webpack.config.js
+++ webpack.config.js
@@ -21,24 +21,9 @@ module.exports = {
 				exclude: '/node_modules',
 				use: [
 					MiniCssExtractPlugin.loader,
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: true,
-						},
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							sourceMap: true,
-						},
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							sourceMap: true,
-						},
-					},
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
 				],
 			},
 			{
