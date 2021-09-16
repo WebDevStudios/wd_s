318c3232bf7af38c41a27158c1a0b9e84e3f0186
Greg Rickaby <gregrickaby@gmail.com>
2021-09-16 07:33:04 -0500

    add function to preload styles and scripts

--- inc/scripts.php
+++ inc/scripts.php
@@ -30,5 +30,36 @@ function _s_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
+
+/**
+ * Preload styles and scripts.
+ *
+ * Because we're using an asset file (which generates a random version number)
+ * to enqueue our theme's styles and scripts, we need to pluck the version
+ * number out of the $wp_styles global variable.
+ *
+ * @author WebDevStudios
+ */
+function _s_preload_scripts() {
+	global $wp_styles;
+
+	// Set defaults.
+	$data    = '';
+	$version = '';
+
+	/**
+	 * Loop over the $wp_styles global variable and pluck the version number.
+	 */
+	foreach ( $wp_styles as $key => $value ) {
+		if ( 'registered' === $key ) {
+			$data    = $value['wd_s']; // Note: the array key needs to match the $handle used when enuqueing the main stylesheet.
+			$version = $data->ver;
+		}
+	}
+	?>
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/build/index.css?ver=<?php echo esc_html( $version ); ?>" as="style">
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/build/index.js?ver=<?php echo esc_html( $version ); ?>" as="script">
+	<?php
+}
+add_action( 'wp_head', '_s_preload_scripts', 1 );
