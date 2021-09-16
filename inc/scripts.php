187342a21b35eccc9a0bcf8f4a6a825467f70bdb
Greg Rickaby <greg@webdevstudios.com>
2021-09-16 10:26:20 -0500

    Feature/#739 preload styles scripts (#740)
    * bump @wordpress/scripts to 18.0.0
    
    * bump copy-webpack-plugin to 9.0.1
    
    * bump css-loader to 6.2.0
    
    * bump css-minimizer-webpack-plugin to 3.0.2
    
    * bump eslint-webpack-plugin to 3.0.1
    
    * bump mini-css-extract-plugin to 2.2.0
    
    * bump sass-loader to 12.1.0
    
    * bump svg-spritemap-webpack-plugin to 4.2.0
    
    * bump stylelint-webpack-plugin to 3.0.1
    
    * bump tailwind to postcss 8 compatible version
    
    * bump postcss-loader to 6.1.1
    
    * allow webpack 5 plugins to be upgraded
    
    * bump composer dependencies
    
    * update for webpack 5 config shape
    
    * remove extra . in CopyPlugin path
    
    * point directly at svg-spritemap-webpack-plugin commit
    
    this commit fixes 175 and is temporary until the fix is merged back into `master`
    
    * dependency bump
    
    * Replaces loaders with asset modules
    
    * Removes file-loader & url-loader from package.json
    
    * Updates mini-css-extract to fix sourcemaps
    
    * Simplifies webpack config around loaders
    
    * add function to preload styles and scripts
    
    * bump @wordpress/scripts to 18.0.1
    
    * use the asset file to determine version
    
    Co-authored-by: Corey Collins <coreymcollins@gmail.com>

--- inc/scripts.php
+++ inc/scripts.php
@@ -30,5 +30,28 @@ function _s_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
+
+/**
+ * Preload styles and scripts.
+ *
+ * @author WebDevStudios
+ */
+function _s_preload_scripts() {
+	$asset_file_path = dirname( __DIR__ ) . '/build/index.asset.php';
+
+	if ( is_readable( $asset_file_path ) ) {
+		$asset_file = include $asset_file_path;
+	} else {
+		$asset_file = [
+			'version'      => '1.0.0',
+			'dependencies' => [ 'wp-polyfill' ],
+		];
+	}
+
+	?>
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/build/index.css?ver=<?php echo esc_html( $asset_file['version'] ); ?>" as="style">
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/build/index.js?ver=<?php echo esc_html( $asset_file['version'] ); ?>" as="script">
+	<?php
+}
+add_action( 'wp_head', '_s_preload_scripts', 1 );
