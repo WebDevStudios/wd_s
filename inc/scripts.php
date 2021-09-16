a5a61b496c7732fa1bfe2f30f8436f75b8c7f629
Greg Rickaby <gregrickaby@gmail.com>
2021-09-16 09:00:27 -0500

    use the asset file to determine version

--- inc/scripts.php
+++ inc/scripts.php
@@ -35,31 +35,23 @@ add_action( 'wp_enqueue_scripts', '_s_scripts' );
 /**
  * Preload styles and scripts.
  *
- * Because we're using an asset file (which generates a random version number)
- * to enqueue our theme's styles and scripts, we need to pluck the version
- * number out of the $wp_styles global variable.
- *
  * @author WebDevStudios
  */
 function _s_preload_scripts() {
-	global $wp_styles;
+	$asset_file_path = dirname( __DIR__ ) . '/build/index.asset.php';
 
-	// Set defaults.
-	$data    = '';
-	$version = '';
-
-	/**
-	 * Loop over the $wp_styles global variable and pluck the version number.
-	 */
-	foreach ( $wp_styles as $key => $value ) {
-		if ( 'registered' === $key ) {
-			$data    = $value['wd_s']; // Note: the array key needs to match the $handle used when enuqueing the main stylesheet.
-			$version = $data->ver;
-		}
+	if ( is_readable( $asset_file_path ) ) {
+		$asset_file = include $asset_file_path;
+	} else {
+		$asset_file = [
+			'version'      => '1.0.0',
+			'dependencies' => [ 'wp-polyfill' ],
+		];
 	}
+
 	?>
-	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/build/index.css?ver=<?php echo esc_html( $version ); ?>" as="style">
-	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/build/index.js?ver=<?php echo esc_html( $version ); ?>" as="script">
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/build/index.css?ver=<?php echo esc_html( $asset_file['version'] ); ?>" as="style">
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/build/index.js?ver=<?php echo esc_html( $asset_file['version'] ); ?>" as="script">
 	<?php
 }
 add_action( 'wp_head', '_s_preload_scripts', 1 );
