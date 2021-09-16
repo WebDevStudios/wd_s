7c9347e8a327b578794e50121145cc7cd6bcdd89
Corey Collins <coreymcollins@gmail.com>
2021-09-16 11:43:38 -0400

    Adds function to get logo URL

--- inc/extras.php
+++ inc/extras.php
@@ -99,3 +99,25 @@ function _s_copyright_year( $atts ) {
 }
 
 add_shortcode( '_s_copyright_year', '_s_copyright_year', 15 );
+
+/**
+ * Retrieve the URL of the custom logo uploaded, if one exists.
+ *
+ * @author Corey Collins
+ */
+function _s_get_custom_logo_url() {
+
+	$custom_logo_id = get_theme_mod( 'custom_logo' );
+
+	if ( ! $custom_logo_id ) {
+		return;
+	}
+
+	$custom_logo_image = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+
+	if ( ! isset( $custom_logo_image[0] ) ) {
+		return;
+	}
+
+	return $custom_logo_image[0];
+}
