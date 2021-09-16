e6a9eaa5caa1834223c262ec568f728fabb918fe
Corey Collins <coreymcollins@gmail.com>
2021-09-16 11:44:52 -0400

    Adds a hook to preload assets (logo, fonts, etc.)

--- inc/scripts.php
+++ inc/scripts.php
@@ -55,3 +55,17 @@ function _s_preload_scripts() {
 	<?php
 }
 add_action( 'wp_head', '_s_preload_scripts', 1 );
+
+/**
+ * Preload assets.
+ *
+ * @author Corey Collins
+ */
+function _s_preload_assets() {
+	?>
+	<?php if ( _s_get_custom_logo_url() ) : ?>
+		<link rel="preload" href="<?php echo esc_url( _s_get_custom_logo_url() ); ?>" as="image">
+	<?php endif; ?>
+	<?php
+}
+add_action( 'wp_head', '_s_preload_assets', 1 );
