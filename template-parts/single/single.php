a74105ce74d6ea7720507b603809c462388e19d5
Corey Collins <coreymcollins@gmail.com>
2021-09-23 10:14:07 -0400

    Creates Template Module files

---
+++ template-parts/single/single.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _s
+ */
+
+?>
+
+	<article <?php post_class( 'post-container' ); ?>>
+
+		<header class="entry-header">
+			<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
+
+			if ( 'post' === get_post_type() ) :
+				?>
+				<div class="entry-meta">
+					<?php _s_posted_on(); ?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php
+				the_content(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. */
+							esc_html__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ),
+							[
+								'span' => [
+									'class' => [],
+								],
+							]
+						),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					)
+				);
+
+				wp_link_pages(
+					[
+						'before' => '<div class="page-links">' . esc_attr__( 'Pages:', '_s' ),
+						'after'  => '</div>',
+					]
+				);
+				?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer">
+			<?php _s_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
+
+	</article><!-- #post-## -->
