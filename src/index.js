8b76ade1bfc90bb57a2d5ed99cf47a202945d274
Corey Collins <coreymcollins@gmail.com>
2021-09-24 11:37:51 -0400

    Updates file imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 /* eslint eslint-comments/disable-enable-pair: error */
 /* eslint-disable no-undef, no-unused-vars */
 
-import './scss/tailwind.scss';
-import './scss/index.scss';
-import './js/index';
+// import './scss/tailwind.scss';
+// import './scss/index.scss';
+// import './js/index';
+
+import './scss/blocks/core/table.css';
 
 /* eslint-enable no-undef, no-unused-vars */
