9b2cfb776e0ae2380d16deff1bb57a24f94c146c
JC <jc@webdevstudios.com>
2021-09-24 22:51:34 +0800

    #747 - Fix markup of scaffolding to add a pseudo-accordion

--- inc/scaffolding.php
+++ inc/scaffolding.php
@@ -34,65 +34,64 @@ function _s_display_scaffolding_section( $args = [] ) {
 	// Add a unique class to the wrapper.
 	$class = 'scaffolding-' . str_replace( ' ', '-', strtolower( $args['title'] ) ); ?>
 
-	<div class="scaffolding-document accordion <?php echo esc_attr( $class ); ?>">
+<div class="scaffolding-document tabs <?php echo esc_attr( $class ); ?>">
 
-		<div class="accordion-item">
-			<?php if ( $args['title'] ) : ?>
-			<header class="scaffolding-document-header display-flex flex-start space-between accordion-item-header">
-				<h3 class="scaffolding-document-title accordion-item-title"><?php echo esc_html( $args['title'] ); ?></h3>
-				<button type="button" class="scaffolding-button"><?php esc_html_e( 'Details', '_s' ); ?></button>
-			</header><!-- .scaffolding-document-header -->
+	<div class="tab">
+		<?php if ( $args['title'] ) : ?>
+			<input type="checkbox" class="tab-toggle" id="<?php echo esc_attr( 'tab-' . $args['title'] ); ?>">
+			<label class="scaffolding-document-title tab-label" for="<?php echo esc_attr( 'tab-' . $args['title'] ); ?>"><?php echo esc_html( $args['title'] ); ?></label>
+		<?php endif; ?>
+
+
+		<div class="scaffolding-document-content tab-content">
+
+			<div class="scaffolding-document-details">
+
+			<?php if ( $args['description'] ) : ?>
+				<p><strong><?php esc_html_e( 'Description', '_s' ); ?>:</strong></p>
+				<p class="scaffolding-document-description"><?php echo esc_html( $args['description'] ); ?></p>
 			<?php endif; ?>
 
-			<div class="scaffolding-document-content accordion-item-content">
+			<?php if ( $args['parameters'] ) : ?>
+				<p><strong><?php esc_html_e( 'Parameters', '_s' ); ?>:</strong></p>
+				<?php foreach ( $args['parameters'] as $key => $value ) : ?>
+					<p><code><?php echo esc_html( $key ); ?></code> <?php echo esc_html( $value ); ?></p>
+				<?php endforeach; ?>
+			<?php endif; ?>
 
-				<div class="scaffolding-document-details">
+			<?php if ( $args['arguments'] ) : ?>
+				<p><strong><?php esc_html_e( 'Arguments', '_s' ); ?>:</strong></p>
+				<?php foreach ( $args['arguments'] as $key => $value ) : ?>
+					<p><code><?php echo esc_html( $key ); ?></code> <?php echo esc_html( $value ); ?></p>
+				<?php endforeach; ?>
+			<?php endif; ?>
 
-				<?php if ( $args['description'] ) : ?>
-					<p><strong><?php esc_html_e( 'Description', '_s' ); ?>:</strong></p>
-					<p class="scaffolding-document-description"><?php echo esc_html( $args['description'] ); ?></p>
-				<?php endif; ?>
+			</div><!-- .scaffolding-document-details -->
 
-				<?php if ( $args['parameters'] ) : ?>
-					<p><strong><?php esc_html_e( 'Parameters', '_s' ); ?>:</strong></p>
-					<?php foreach ( $args['parameters'] as $key => $value ) : ?>
-						<p><code><?php echo esc_html( $key ); ?></code> <?php echo esc_html( $value ); ?></p>
-					<?php endforeach; ?>
-				<?php endif; ?>
+			<div class="scaffolding-document-usage">
 
-				<?php if ( $args['arguments'] ) : ?>
-					<p><strong><?php esc_html_e( 'Arguments', '_s' ); ?>:</strong></p>
-					<?php foreach ( $args['arguments'] as $key => $value ) : ?>
-						<p><code><?php echo esc_html( $key ); ?></code> <?php echo esc_html( $value ); ?></p>
-					<?php endforeach; ?>
-				<?php endif; ?>
+			<?php if ( $args['usage'] ) : ?>
+				<p><strong><?php esc_html_e( 'Usage', '_s' ); ?>:</strong></p>
+				<pre><?php echo esc_html( $args['usage'] ); ?></pre>
+			<?php endif; ?>
 
-				</div><!-- .scaffolding-document-details -->
+			<?php if ( $args['output'] ) : ?>
+				<p><strong><?php esc_html_e( 'HTML Output', '_s' ); ?>:</strong></p>
+				<pre><?php echo esc_html( $args['output'] ); ?></pre>
+			<?php endif; ?>
 
-				<div class="scaffolding-document-usage">
+			</div><!-- .scaffolding-document-usage -->
+		</div><!-- .scaffolding-document-content -->
+	</div>
 
-				<?php if ( $args['usage'] ) : ?>
-					<p><strong><?php esc_html_e( 'Usage', '_s' ); ?>:</strong></p>
-					<pre><?php echo esc_html( $args['usage'] ); ?></pre>
-				<?php endif; ?>
-
-				<?php if ( $args['output'] ) : ?>
-					<p><strong><?php esc_html_e( 'HTML Output', '_s' ); ?>:</strong></p>
-					<pre><?php echo esc_html( $args['output'] ); ?></pre>
-				<?php endif; ?>
-
-				</div><!-- .scaffolding-document-usage -->
-			</div><!-- .scaffolding-document-content -->
-		</div>
-
-		<div class="scaffolding-document-live">
+	<div class="scaffolding-document-live">
 
 		<?php if ( $args['output'] ) : ?>
 			<?php echo do_shortcode( wp_kses( $args['output'], $allowed_tags ) ); ?>
 		<?php endif; ?>
 
-		</div><!-- .scaffolding-document-live -->
-	</div><!-- .scaffolding-document -->
+	</div><!-- .scaffolding-document-live -->
+</div><!-- .scaffolding-document -->
 
 	<?php
 }
@@ -199,10 +198,11 @@ function _s_display_global_scaffolding_section( $args = [] ) {
 					$colors = $args['arguments'];
 
 					foreach ( $colors as $name => $hex ) :
-						$color_var = '$color-' . str_replace( ' ', '-', strtolower( $name ) );
+						$bg_class  = 'bg-' . str_replace( ' ', '_', strtolower( $name ) );
+						$color_var = '.' . $bg_class . ' ( ' . strtolower( $hex ) . ' )';
 						?>
 
-						<div class="swatch quarter" style="background-color: <?php echo esc_attr( $hex ); ?>;">
+						<div class="swatch quarter <?php echo esc_attr( $bg_class ); ?>" style="background-color: <?php echo esc_attr( $hex ); ?>;">
 							<header><?php echo esc_html( $name ); ?></header>
 							<footer><?php echo esc_html( $color_var ); ?></footer>
 						</div><!-- .swatch -->
