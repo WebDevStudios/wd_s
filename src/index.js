95eb949bd732b05b56793d54569a611de8d3cdac
Corey Collins <coreymcollins@gmail.com>
2021-09-30 16:07:55 -0400

    Updates our scripts/includes/imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './js/index';
 import './css/blocks/core/_index.css';
 import './css/global/global.css';
 import './css/template-tags/_index.css';
 import './css/templates/_index.css';
 import './css/wordpress-global/_index.css';
+import './js/index';
