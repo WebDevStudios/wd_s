994ee6282ebf5328ac2121f0dfdf6789d36d4890
Corey Collins <coreymcollins@gmail.com>
2021-10-05 11:27:28 -0400

    Merges master in

--- inc/scripts.php
+++ inc/scripts.php
@@ -30,5 +30,42 @@ function _s_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
+
+/**
+ * Preload styles and scripts.
+ *
+ * @author WebDevStudios
+ */
+function _s_preload_scripts() {
+	$asset_file_path = dirname( __DIR__ ) . '/build/index.asset.php';
+
+	if ( is_readable( $asset_file_path ) ) {
+		$asset_file = include $asset_file_path;
+	} else {
+		$asset_file = [
+			'version'      => '1.0.0',
+			'dependencies' => [ 'wp-polyfill' ],
+		];
+	}
+
+	?>
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/build/index.css?ver=<?php echo esc_html( $asset_file['version'] ); ?>" as="style">
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/build/index.js?ver=<?php echo esc_html( $asset_file['version'] ); ?>" as="script">
+	<?php
+}
+add_action( 'wp_head', '_s_preload_scripts', 1 );
+
+/**
+ * Preload assets.
+ *
+ * @author Corey Collins
+ */
+function _s_preload_assets() {
+	?>
+	<?php if ( _s_get_custom_logo_url() ) : ?>
+		<link rel="preload" href="<?php echo esc_url( _s_get_custom_logo_url() ); ?>" as="image">
+	<?php endif; ?>
+	<?php
+}
+add_action( 'wp_head', '_s_preload_assets', 1 );
