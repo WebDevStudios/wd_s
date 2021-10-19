af7ad8aa57ff0b9ab69fe34441e98a81637347b1
Adam <adam@bateswebdesign.com>
2021-10-19 16:07:03 -0400

    Remove other dev function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -59,35 +59,6 @@ function _s_post_author( $args = [] ) {
 	<?php
 }
 
-function _s_display_post_meta( $first = 'date', $date_meta = [], $byline_meta = [] ) {
-
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
-
-	$time_string = sprintf(
-		$time_string,
-		esc_attr( get_the_date( DATE_W3C ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( DATE_W3C ) ),
-		esc_html( get_the_modified_date() )
-	);
-
-	$date = sprintf(
-		/* translators: the date the post was published */
-		esc_html_x( 'Posted on %s', 'post date', '_s' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
-
-	$byline = sprintf(
-		/* translators: the post author */
-		esc_html_x( 'by %s', 'post author', '_s' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-}
-
 /**
  * Prints HTML with meta information for the categories, tags and comments.
  *
