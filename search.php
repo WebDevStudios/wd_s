a8a8d970214e82905e442e5fb09521e70e493fe8
Adam <adam@bateswebdesign.com>
2021-12-30 10:41:40 -0500

    Remove header from search.php

--- search.php
+++ search.php
@@ -13,14 +13,6 @@ get_header(); ?>
 
 		<?php
 		if ( have_posts() ) :
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-
-				<?php
-			endif;
 
 			/* Start the Loop */
 			while ( have_posts() ) :
