266d63dabff0b273cb83062ef3e84a7bd7abaf4f
Adam <adam@bateswebdesign.com>
2022-05-20 08:58:11 -0400

    Renames theme get_the_title function

--- inc/functions/get-title.php
+++ inc/functions/get-title.php
@@ -16,7 +16,7 @@ namespace WebDevStudios\wd_s;
  *
  * @return string The title.
  */
-function get_the_title( $args = [] ) {
+function get_title( $args = [] ) {
 	// Set defaults.
 	$defaults = [
 		'length' => 12,
