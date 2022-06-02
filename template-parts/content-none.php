9b776149beb49350d6bd55ac23549176a090fba8
Oliver Harrison <oliver@positivebias.com>
2022-06-02 08:49:32 -0400

    Update spacing

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -11,35 +11,35 @@
 
 <section class="no-results not-found">
 
-		<header class="page-header">
-			<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'wd_s' ); ?></h1>
-		</header><!-- .page-header -->
+	<header class="page-header">
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'wd_s' ); ?></h1>
+	</header><!-- .page-header -->
 
-		<div class="page-content">
-			<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-				<p>
-				<?php
-				printf(
-					wp_kses(
-						/* translators: the edit post url */
-						esc_html__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'wd_s' ),
-						[
-							'a' => [
-								'href' => [],
-							],
-						]
-					),
-					esc_url( admin_url( 'post-new.php' ) )
-				);
-				?>
-				</p>
-			<?php elseif ( is_search() ) : ?>
-				<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'wd_s' ); ?></p>
-				<?php get_search_form(); ?>
-			<?php else : ?>
-				<p><?php esc_html_e( 'It seems we can\'t find what you\'re looking for. Perhaps searching can help.', 'wd_s' ); ?></p>
-				<?php get_search_form(); ?>
-			<?php endif; ?>
-		</div><!-- .page-content -->
+	<div class="page-content">
+		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+			<p>
+			<?php
+			printf(
+				wp_kses(
+					/* translators: the edit post url */
+					esc_html__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'wd_s' ),
+					[
+						'a' => [
+							'href' => [],
+						],
+					]
+				),
+				esc_url( admin_url( 'post-new.php' ) )
+			);
+			?>
+			</p>
+		<?php elseif ( is_search() ) : ?>
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'wd_s' ); ?></p>
+			<?php get_search_form(); ?>
+		<?php else : ?>
+			<p><?php esc_html_e( 'It seems we can\'t find what you\'re looking for. Perhaps searching can help.', 'wd_s' ); ?></p>
+			<?php get_search_form(); ?>
+		<?php endif; ?>
+	</div><!-- .page-content -->
 
 </section><!-- .no-results -->
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -19,29 +19,29 @@
 		<?php
 		the_content();
 
-				wp_link_pages(
-					[
-						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wd_s' ),
-						'after'  => '</div>',
-					]
-				);
-				?>
-		</div><!-- .entry-content -->
+		wp_link_pages(
+			[
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wd_s' ),
+				'after'  => '</div>',
+			]
+		);
+		?>
+	</div><!-- .entry-content -->
 
-		<?php if ( get_edit_post_link() ) : ?>
-			<footer class="entry-footer">
-				<?php
-					edit_post_link(
-						sprintf(
-							/* translators: %s: Name of current post */
-							esc_html__( 'Edit %s', 'wd_s' ),
-							the_title( '<span class="screen-reader-text">"', '"</span>', false )
-						),
-						'<span class="edit-link">',
-						'</span>'
-					);
-				?>
-			</footer><!-- .entry-footer -->
-		<?php endif; ?>
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+				edit_post_link(
+					sprintf(
+						/* translators: %s: Name of current post */
+						esc_html__( 'Edit %s', 'wd_s' ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
 
 </article><!-- #post-## -->
