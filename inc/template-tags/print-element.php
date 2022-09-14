d319d57fa08e3001916fcec0cdcdeae5eb7cfb82
bplv112 <bplv.me@gmail.com>
2022-09-13 19:21:34 -0500

    fix :: Errors that is caused by adding an optional param ahead of a required param.

--- inc/template-tags/print-element.php
+++ inc/template-tags/print-element.php
@@ -15,6 +15,6 @@ namespace WebDevStudios\wd_s;
  * @param string $element_name The name of the block.
  * @param array  $args Args for the block.
  */
-function print_element( $element_name = '', $args ) {
+function print_element( $element_name = '', $args = [] ) {
 	get_template_part( 'template-parts/acf-blocks/elements/' . $element_name, '', $args );
 }
