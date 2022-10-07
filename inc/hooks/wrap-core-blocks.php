09d0a20393b3734a9a1a6bcd650bb9c3ce2c0959
Amor Kumar <contact@amorkumar.com>
2022-10-07 10:17:51 -0400

    Wrap core blocks with wp-block-* classes
    Some core blocks have these classes, some don't. This will wrap the core blocks we support that don't have these classes with wp-block-* classes.

---
+++ inc/hooks/wrap-core-blocks.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Wrap core blocks with a class.
+ *
+ * @package wd_s
+ */
+
+namespace WebDevStudios\wd_s;
+
+/**
+ * Wrap core blocks with a wp-block-* class.
+ *
+ * Not all core blocks have these classes. This function will add these classes
+ * to core blocks that don't have them.
+ *
+ * @author WebDevStudios
+ *
+ * @param string $block_content The content of the block (what will render).
+ * @param string $block         The block's name.
+ */
+function wrap_core_blocks( $block_content, $block ) {
+	$blocks_to_wrap = [
+		[ 'core/heading', 'heading' ],
+		[ 'core/paragraph', 'paragraph' ],
+		[ 'core/freeform', 'freeform' ],
+		[ 'core/html', 'html' ],
+		[ 'core/list', 'list' ],
+		[ 'core/table', 'table' ],
+	];
+
+	foreach ( $blocks_to_wrap as $block_to_wrap ) {
+		if ( $block_to_wrap[0] === $block['blockName'] ) {
+			$block_content = '<span class="wp-block-' . $block_to_wrap[1] . '">' . $block_content . '</span>';
+		}
+	}
+	return $block_content;
+}
+
+add_filter( 'render_block', __NAMESPACE__ . '\wrap_core_blocks', 10, 2 );
