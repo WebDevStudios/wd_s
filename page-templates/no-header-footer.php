bb66bfcb756b101f38166c7e33128ea9b35e7f07
DanhThong <danhthong@gmail.com>
2023-01-01 02:25:53 +0700

    feat: COWP-62 add no header no footer template

---
+++ page-templates/no-header-footer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template Name: No Header, No Footer
+ *
+ * This template displays a page without header and footer.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package wd_s
+ */
+
+use function WebDevStudios\wd_s\print_comments;
+use function WebDevStudios\wd_s\main_classes;
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<?php wp_head(); ?>
+	</head>
+
+	<div class="<?php echo esc_attr( main_classes( [] ) ); ?>">
+		<main id="main" class="content-container">
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				print_comments();
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+
+	</div>
+	<?php wp_footer(); ?>
+
+</body>
+</html>
