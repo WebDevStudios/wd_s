9b13412f37a72be20e43f91eb7fd7dcf19a09595
JC <jc@webdevstudios.com>
2023-04-29 03:41:40 +0800

    WDSUS-5 - Fix undefined var error

--- searchform.php
+++ searchform.php
@@ -10,9 +10,9 @@ $wd_s_random_identifier = wp_rand();
 ?>
 
 <form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="search-field-<?php echo esc_attr( $random_identifier ); ?>">
+	<label for="search-field-<?php echo esc_attr( $wd_s_random_identifier ); ?>">
 		<span class="screen-reader-text"><?php esc_html_e( 'To search this site, enter a search term', 'wd_s' ); ?></span>
-		<input class="search-field" id="search-field-<?php echo esc_attr( $random_identifier ); ?>" type="text" name="s" value="<?php echo get_search_query(); ?>" aria-required="false" autocomplete="off" placeholder="<?php echo esc_attr_e( 'Search', 'wd_s' ); ?>" />
+		<input class="search-field" id="search-field-<?php echo esc_attr( $wd_s_random_identifier ); ?>" type="text" name="s" value="<?php echo get_search_query(); ?>" aria-required="false" autocomplete="off" placeholder="<?php echo esc_attr_e( 'Search', 'wd_s' ); ?>" />
 	</label>
 	<input type="submit" id="search-submit" class="button button-search" tabindex="-1" value="<?php esc_attr_e( 'Submit', 'wd_s' ); ?>" />
 </form>
