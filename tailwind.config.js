3095ac132ef124fb46e7f36c25144c6ac77fe3e0
JC <jc@webdevstudios.com>
2023-05-18 03:55:16 +0800

    WDSUS-5 - Remove scaffolding template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const fs = require( 'fs' );
+const path = require( 'path' );
 const glob = require( 'glob' );
 
 // Get arrays of all of the files.
@@ -8,6 +10,28 @@ const topLevelPhpFiles = glob.sync( './*.php' ),
 		'./src/js/**/*.js',
 	];
 
+const themeJsonPath = path.join( __dirname, 'theme.json' );
+const themeJson = fs.readFileSync( themeJsonPath );
+const theme = JSON.parse( themeJson );
+
+const { palette } = theme.settings.color;
+const colors = palette.reduce( ( acc, item ) => {
+	const [ color, number ] = item.slug.split( '-' );
+
+	if ( number ) {
+		// If there is a number identifier, make this an object
+		if ( ! acc[ color ] ) {
+			acc[ color ] = {};
+		}
+		acc[ color ][ number ] = item.color;
+	} else {
+		acc[ color ] = item.color;
+	}
+
+	return acc;
+}, {} );
+
 module.exports = {
 	content: topLevelPhpFiles.concat( directoryFiles ),
+	theme: { colors },
 };
