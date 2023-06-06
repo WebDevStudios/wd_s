a8ea7f619165d82513583eca9426ba139633ee85
JC <jc@webdevstudios.com>
2023-06-07 02:03:51 +0800

    WDSUS-26 - Add sample CTA pattern

---
+++ patterns/cta.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: Call to Action
+ * Slug: wds/cta
+ * Categories: wds-patterns
+ * Viewport Width: 1280
+ * Block Types: core/template-part/cta
+ * Inserter: yes
+ *
+ * @package wd_s
+ */
+
+?>
+<!-- wp:group {"align":"full","backgroundColor":"primary-300","textColor":"secondary-100"} -->
+<div class="wp-block-group alignfull has-secondary-100-color has-primary-300-background-color has-text-color has-background"><!-- wp:spacer {"height":"64px"} -->
+<div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":".9"}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="line-height:.9"><strong>EYEBROW</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":59,"lineHeight":"1.15"}}} -->
+<h2 class="wp-block-heading has-text-align-center" id="schedule-a-visit" style="font-size:59px;line-height:1.15"><strong>Call to Action</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"tertiary-300","width":50,"style":{"color":{"text":"#ffffff"},"border":{"radius":"50px"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-50"><a class="wp-block-button__link has-tertiary-300-background-color has-text-color has-background wp-element-button" style="border-radius:50px;color:#ffffff">Contact us</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer {"height":"64px"} -->
+<div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
