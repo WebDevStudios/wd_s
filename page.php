21a0f644a5d2b1050cfc1a70840ea752340ef33a
JC <jc@webdevstudios.com>
2023-09-21 04:28:24 +0800

    WDSUS-45 - Add page default template

---
+++ page.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package wd_s
+ */
+
+use function WebDevStudios\wd_s\print_numeric_pagination;
+use function WebDevStudios\wd_s\main_classes;
+
+get_header(); ?>
+	<div class="wp-site-blocks">
+		<main id="main" class="<?php echo esc_attr( main_classes( [] ) ); ?>">
+
+			<?php
+			if ( have_posts() ) :
+				if ( is_home() && ! is_front_page() ) :
+					?>
+					<header class="entry-header is-layout-constrained">
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+
+					<?php
+				endif;
+
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+
+					get_template_part( 'template-parts/content', get_post_type() );
+
+				endwhile;
+
+				print_numeric_pagination();
+
+			else :
+				get_template_part( 'template-parts/content', 'none' );
+			endif;
+			?>
+
+		</main><!-- #main -->
+	</div><!-- .wp-site-blocks -->
+
+<?php get_footer(); ?>
