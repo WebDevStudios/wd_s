6344fdca7f77cc52a9fdc393c59b1a383633e8e1
JC <jc@webdevstudios.com>
2023-09-21 04:47:49 +0800

    WDSUS-45 - Remove wd_s, already present in namespace

--- inc/template-tags/print-footer-block.php
+++ inc/template-tags/print-footer-block.php
@@ -19,33 +19,33 @@ namespace WebDevStudios\wd_s;
  */
 function print_footer_block() {
 	// Determine whether to display site logo or site title.
-	$wds_site_info = has_custom_logo() ? '<!-- wp:site-logo {"align":"center"} /-->' : '<!-- wp:site-title {"textAlign":"center"} /-->';
+	$site_info = has_custom_logo() ? '<!-- wp:site-logo {"align":"center"} /-->' : '<!-- wp:site-title {"textAlign":"center"} /-->';
 
 	// Generate the copyright information.
-	$wds_copyright_info = esc_html__( ' Copyright &copy; ', 'wd_s' ) . esc_attr( gmdate( 'Y' ) ) . esc_html__( '. All Rights Reserved.', 'wd_s' );
+	$copyright_info = esc_html__( ' Copyright &copy; ', 'wd_s' ) . esc_attr( gmdate( 'Y' ) ) . esc_html__( '. All Rights Reserved.', 'wd_s' );
 
 	// Generate the theme attribution.
-	$wds_theme_info = esc_html__( ' Proudly powered by WordPress. ', 'wd_s' ) . '<a href="' . esc_url( 'https://webdevstudios.com/' ) . '">' . esc_html__( 'WebDevStudios', 'wd_s' ) . '</a>' . esc_html__( ' is a WD3 Party.', 'wd_s' );
+	$theme_info = esc_html__( ' Proudly powered by WordPress. ', 'wd_s' ) . '<a href="' . esc_url( 'https://webdevstudios.com/' ) . '">' . esc_html__( 'WebDevStudios', 'wd_s' ) . '</a>' . esc_html__( ' is a WD3 Party.', 'wd_s' );
 
 	// Generate the footer block content.
-	$wds_footer_content = <<<HTML
+	$footer_content = <<<HTML
     <!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
     <div class="wp-block-group alignwide">
-        $wds_site_info
+        $site_info
 
         <!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"var:preset|spacing|20","bottom":"0"}}},"fontSize":"small"} -->
-        <p class="has-text-align-center has-small-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:0;padding-top:0;padding-bottom:0">$wds_copyright_info</p>
+        <p class="has-text-align-center has-small-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:0;padding-top:0;padding-bottom:0">$copyright_info</p>
         <!-- /wp:paragraph -->
 
         <!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"var:preset|spacing|20","bottom":"0"}}},"fontSize":"small"} -->
-        <p class="has-text-align-center has-small-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:0;padding-top:0;padding-bottom:0">$wds_theme_info</p>
+        <p class="has-text-align-center has-small-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:0;padding-top:0;padding-bottom:0">$theme_info</p>
         <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
     HTML;
 
 	// Render the block content using do_blocks() function.
-	$wds_block_footer = do_blocks( $wds_footer_content );
+	$block_footer = do_blocks( $footer_content );
 
-	return $wds_block_footer;
+	return $block_footer;
 }
