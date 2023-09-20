dd0a6ae80a20ddcba47e7e8d7d9486017b825b50
JC <jc@webdevstudios.com>
2023-09-21 05:10:55 +0800

    WDSUS-45 - Add default home and front page templates

---
+++ home.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying your latest blog posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#home-page-display
+ *
+ * @package wd_s
+ */
+
+use function WebDevStudios\wd_s\print_numeric_pagination;
+use function WebDevStudios\wd_s\main_classes;
+
+get_header(); ?>
+	<div class="wp-site-blocks">
+		<main id="main" class="<?php echo esc_attr( main_classes( [] ) ); ?>">
+
+			<?php
+			if ( have_posts() ) :
+				if ( is_home() && ! is_front_page() ) :
+					?>
+					<header class="entry-header is-layout-constrained">
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+
+					<?php
+				endif;
+
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+
+					get_template_part( 'template-parts/content', get_post_type() );
+
+				endwhile;
+
+				print_numeric_pagination();
+
+			else :
+				get_template_part( 'template-parts/content', 'none' );
+			endif;
+			?>
+
+		</main><!-- #main -->
+	</div><!-- .wp-site-blocks -->
+
+<?php get_footer(); ?>
